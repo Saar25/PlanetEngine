f6e176a0b4d4c1e52427055f2b220d60e227240c
Saar <notnow@maybelater.com>
2020-06-12 15:37:08 +0300

    Imported class PNGDecoder

--- pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/textures/TextureLoader.java
+++ pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/textures/TextureLoader.java
@@ -1,6 +1,7 @@
 package org.saar.lwjgl.opengl.textures;
 
 import engine.fileLoaders.PNGDecoder;
+import org.saar.file.PNGDecoder;
 import org.saar.lwjgl.opengl.utils.MemoryUtils;
 
 import java.nio.ByteBuffer;
