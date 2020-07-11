56eec7b8e862831750e02c669e7c923e55c783d3
Saar <notnow@maybelater.com>
2020-07-11 12:00:04 +0300

    Implemented Model creation

--- pe-core/src/main/java/org/saar/core/model/Models.java
+++ pe-core/src/main/java/org/saar/core/model/Models.java
@@ -1,5 +1,11 @@
 package org.saar.core.model;
 
+import org.saar.core.model.vertex.Vertex;
+import org.saar.core.model.vertex.VerticesBuffer;
+import org.saar.lwjgl.opengl.constants.RenderMode;
+
+import java.util.Arrays;
+
 public final class Models {
 
     private Models() {
@@ -7,5 +13,15 @@ public final class Models {
                 + getClass().getSimpleName());
     }
 
-    public static Model create(int[] indices)
+    public static Model arraysModel(RenderMode renderMode, VerticesBuffer buffer, Vertex... vertices) {
+        Arrays.stream(vertices).forEach(vertex -> vertex.write(buffer));
+        return new ArraysModel(buffer.vao(), vertices.length, renderMode);
+    }
+
+    public static Model elementsModel(RenderMode renderMode, VerticesBuffer buffer,
+                                      int[] indices, Vertex... vertices) {
+        Arrays.stream(indices).forEach(buffer::writeIndex);
+        Arrays.stream(vertices).forEach(vertex -> vertex.write(buffer));
+        return new ElementsModel(buffer.vao(), indices.length, renderMode);
+    }
 }
