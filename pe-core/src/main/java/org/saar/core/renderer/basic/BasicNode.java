36d4e856c76f454cc3d582f284bfb5dfc8fb4ef6
Saar <notnow@maybelater.com>
2020-07-18 16:02:04 +0300

    Refactored to interface

--- pe-core/src/main/java/org/saar/core/renderer/basic/BasicNode.java
+++ pe-core/src/main/java/org/saar/core/renderer/basic/BasicNode.java
@@ -1,18 +1,10 @@
 package org.saar.core.renderer.basic;
 
 import org.saar.core.model.vertex.ModelVertices;
-import org.saar.core.node.AbstractNode;
 import org.saar.core.node.Node;
 
-public class BasicNode extends AbstractNode implements Node {
+public interface BasicNode extends Node {
 
-    private final ModelVertices<BasicVertex> vertices;
+    ModelVertices<? extends BasicVertex> getVertices();
 
-    public BasicNode(ModelVertices<BasicVertex> vertices) {
-        this.vertices = vertices;
-    }
-
-    public ModelVertices<BasicVertex> getVertices() {
-        return this.vertices;
-    }
 }
