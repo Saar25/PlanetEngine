aafa37b674d265ad15eff1a6a307f6216cbfdffc
Saar <notnow@maybelater.com>
2020-07-25 14:59:45 +0300

    Converted to kotlin

--- pe-core/src/main/java/org/saar/core/renderer/basic/BasicRenderer.kt
+++ pe-core/src/main/java/org/saar/core/renderer/basic/BasicRenderer.kt
@@ -1,37 +1,31 @@
-package org.saar.core.renderer.basic;
+package org.saar.core.renderer.basic
 
-import org.saar.core.renderer.Renderer;
-import org.saar.lwjgl.opengl.shaders.Shader;
-import org.saar.lwjgl.opengl.shaders.ShadersProgram;
+import org.saar.core.renderer.Renderer
+import org.saar.lwjgl.opengl.shaders.Shader
+import org.saar.lwjgl.opengl.shaders.ShadersProgram
 
-public class BasicRenderer implements Renderer {
+class BasicRenderer(private val renderNode: BasicRenderNode) : Renderer {
 
-    private static final String vertex = "/shaders/basic/vertex.glsl";
-    private static final String fragment = "/shaders/basic/fragment.glsl";
-
-    private final BasicRenderNode renderNode;
-    private final ShadersProgram shadersProgram;
-
-    public BasicRenderer(BasicRenderNode renderNode) throws Exception {
-        this.shadersProgram = ShadersProgram.create(
-                Shader.createVertex(vertex),
-                Shader.createFragment(fragment));
-        this.renderNode = renderNode;
-
-        this.shadersProgram.bindAttributes("in_position", "in_colour");
+    companion object {
+        private val vertex: Shader = Shader.createVertex(
+                "/shaders/basic/vertex.glsl")
+        private val fragment: Shader = Shader.createFragment(
+                "/shaders/basic/fragment.glsl")
+        private val shadersProgram: ShadersProgram =
+                ShadersProgram.create(vertex, fragment)
     }
 
-    @Override
-    public void render() {
-        this.shadersProgram.bind();
-
-        this.renderNode.getModel().draw();
-
-        this.shadersProgram.unbind();
+    init {
+        shadersProgram.bindAttributes("in_position", "in_colour")
     }
 
-    @Override
-    public void delete() {
-        this.shadersProgram.delete();
+    override fun render() {
+        shadersProgram.bind()
+        renderNode.model.draw()
+        shadersProgram.unbind()
+    }
+
+    override fun delete() {
+        shadersProgram.delete()
     }
 }
