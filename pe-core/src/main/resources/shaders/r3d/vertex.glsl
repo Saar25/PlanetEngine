713bfbfa68c702f3d5e20146b23b99903c712b0b
Saar <notnow@maybelater.com>
2020-08-21 12:17:50 +0300

    Created r3d shaders

---
+++ pe-core/src/main/resources/shaders/r3d/vertex.glsl
@@ -0,0 +1,19 @@
+#version 400
+
+// Per Vertex attibutes
+layout (location = 0) in vec3 in_position;
+layout (location = 1) in vec3 in_colour;
+layout (location = 2) in mat4 in_transformation;
+
+// Uniforms
+uniform mat4 viewProjectionMatrix;
+
+// Vertex outputs
+out vec3 v_colour;
+
+void main(void) {
+    v_colour = in_colour;
+
+    const vec4 world = in_transformation * vec4(in_position, 1.0);
+    gl_Position = viewProjectionMatrix * world;
+}
