2d76d251cc68a983d474d99c710394c47dec19a8
Saar <notnow@maybelater.com>
2020-08-21 13:53:51 +0300

    Created InstancedModelExample

--- planet-examples/src/main/resources/vertex.glsl
+++ planet-examples/src/main/resources/vertex.glsl
@@ -3,6 +3,7 @@
 // Per Vertex attibutes
 layout (location = 0) in vec2 in_position;
 layout (location = 1) in vec3 in_colour;
+layout (location = 2) in float in_offset;
 
 // Vertex outputs
 out vec2 v_position;
@@ -10,7 +11,7 @@ out vec3 v_colour;
 
 void main(void) {
     v_position = in_position;
-    v_colour = in_colour;
+    v_colour = in_colour + in_offset;
 
-    gl_Position = vec4(in_position, 0.0, 1.0);
+    gl_Position = vec4(in_position + in_offset, 0.0, 1.0);
 }
