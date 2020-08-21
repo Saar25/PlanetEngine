34d8a2e5b2dfb2c5979b9bef1426371eee76643c
Saar <notnow@maybelater.com>
2020-08-21 13:17:20 +0300

    Created Renderers util class

---
+++ pe-core/src/main/java/org/saar/core/renderer/Renderers.java
@@ -0,0 +1,20 @@
+package org.saar.core.renderer;
+
+import org.saar.lwjgl.opengl.shaders.uniforms.UniformProperty;
+
+import java.util.List;
+
+public final class Renderers {
+
+    public Renderers() {
+        throw new AssertionError("Cannot create instance of class " + getClass().getSimpleName());
+    }
+
+    public static List<UniformProperty<?>> findStageUniformProperties(Renderer renderer) {
+        return new UniformPropertiesLocator(renderer).getStageUniformProperties();
+    }
+
+    public static List<UniformProperty<?>> findInstanceUniformProperties(Renderer renderer) {
+        return new UniformPropertiesLocator(renderer).getInstanceUniformProperties();
+    }
+}
