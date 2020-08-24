91dbe480b0560bff094ddb9b26cd816655a30140
Saar <notnow@maybelater.com>
2020-08-24 19:23:50 +0300

    Created Fbo exceptions

---
+++ pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/fbos/FboStatus.java
@@ -0,0 +1,33 @@
+package org.saar.lwjgl.opengl.fbos;
+
+import org.lwjgl.opengl.GL30;
+import org.lwjgl.opengl.GL32;
+import org.saar.lwjgl.opengl.fbos.exceptions.*;
+
+public final class FboStatus {
+
+    private static final String MESSAGE = "Framebuffer creation failed";
+
+    public void checkStatus(int status) throws FrameBufferException {
+        // GL30.glCheckFramebufferStatus(GL30.GL_FRAMEBUFFER)
+        switch (status) {
+            case GL30.GL_FRAMEBUFFER_UNDEFINED:
+                throw new FboUndefinedException(MESSAGE);
+            case GL30.GL_FRAMEBUFFER_INCOMPLETE_ATTACHMENT:
+                throw new FboIncompleteAttachmentException(MESSAGE);
+            case GL30.GL_FRAMEBUFFER_INCOMPLETE_MISSING_ATTACHMENT:
+                throw new FboIncompleteMissingAttachmentException(MESSAGE);
+            case GL30.GL_FRAMEBUFFER_INCOMPLETE_DRAW_BUFFER:
+                throw new FboIncompleteDrawBufferException(MESSAGE);
+            case GL30.GL_FRAMEBUFFER_INCOMPLETE_READ_BUFFER:
+                throw new FboIncompleteReadBufferException(MESSAGE);
+            case GL30.GL_FRAMEBUFFER_UNSUPPORTED:
+                throw new FboUnsupportedException(MESSAGE);
+            case GL30.GL_FRAMEBUFFER_INCOMPLETE_MULTISAMPLE:
+                throw new FboIncompleteMultisampleException(MESSAGE);
+            case GL32.GL_FRAMEBUFFER_INCOMPLETE_LAYER_TARGETS:
+                throw new FboIncompleteLayerTargetsException(MESSAGE);
+        }
+    }
+
+}
