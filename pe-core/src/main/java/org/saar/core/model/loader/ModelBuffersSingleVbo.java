6ec6f0d3d8332946bd728efd2090d8b068d06a50
Saar <notnow@maybelater.com>
2020-08-28 19:07:56 +0300

    Fixed

--- pe-core/src/main/java/org/saar/core/model/loader/ModelBuffersSingleVbo.java
+++ pe-core/src/main/java/org/saar/core/model/loader/ModelBuffersSingleVbo.java
@@ -1,6 +1,8 @@
 package org.saar.core.model.loader;
 
-import org.saar.core.model.*;
+import org.saar.core.model.InstancedElementsModel;
+import org.saar.core.model.Model;
+import org.saar.core.model.Vertex;
 import org.saar.core.node.Node;
 import org.saar.lwjgl.opengl.constants.DataType;
 import org.saar.lwjgl.opengl.constants.RenderMode;
@@ -8,12 +10,9 @@ import org.saar.lwjgl.opengl.constants.VboUsage;
 import org.saar.lwjgl.opengl.objects.Attribute;
 import org.saar.lwjgl.opengl.objects.DataBuffer;
 import org.saar.lwjgl.opengl.objects.IndexBuffer;
-import org.saar.lwjgl.opengl.objects.Vao;
 import org.saar.lwjgl.opengl.utils.BufferWriter;
 
 public abstract class ModelBuffersSingleVbo<N extends Node, V extends Vertex> extends AbstractModelBuffers<N, V> {
-
-    protected final Vao vao = Vao.create();
 
     private final ModelBuffer instanceBuffer;
     private final ModelBuffer vertexBuffer;
@@ -22,23 +21,13 @@ public abstract class ModelBuffersSingleVbo<N extends Node, V extends Vertex> ex
     private final Model model;
 
     public ModelBuffersSingleVbo(int vertices, int indices, int instances, Attribute[] vertexAttributes, Attribute[] instanceAttributes) {
-        this.instanceBuffer = instances == 0 ? null : loadDataBuffer(
-                new DataBuffer(VboUsage.STATIC_DRAW), instances, instanceAttributes);
-        this.vertexBuffer = vertices == 0 ? null : loadDataBuffer(
-                new DataBuffer(VboUsage.STATIC_DRAW), vertices, vertexAttributes);
-        this.indexBuffer = indices == 0 ? null : loadIndexBuffer(
-                new IndexBuffer(VboUsage.STATIC_DRAW), indices);
+        this.instanceBuffer = loadDataBuffer(new DataBuffer(VboUsage.STATIC_DRAW),
+                instances, instanceAttributes);
+        this.vertexBuffer = loadDataBuffer(new DataBuffer(VboUsage.STATIC_DRAW),
+                vertices, vertexAttributes);
+        this.indexBuffer = loadIndexBuffer(new IndexBuffer(VboUsage.STATIC_DRAW), indices);
 
-        this.model = createModel(vertices, indices, instances);
-    }
-
-    private Model createModel(int vertices, int indices, int instances) {
-        if (instances > 0) return indices > 0
-                ? new InstancedElementsModel(this.vao, RenderMode.TRIANGLES, indices, DataType.U_INT, instances)
-                : new InstancedArraysModel(this.vao, RenderMode.TRIANGLES, vertices, instances);
-        else return indices > 0
-                ? new ElementsModel(this.vao, RenderMode.TRIANGLES, indices, DataType.U_INT)
-                : new ArraysModel(this.vao, RenderMode.TRIANGLES, vertices);
+        this.model = new InstancedElementsModel(this.vao, RenderMode.TRIANGLES, indices, DataType.U_INT, instances);
     }
 
     protected final BufferWriter instanceWriter() {
@@ -54,15 +43,9 @@ public abstract class ModelBuffersSingleVbo<N extends Node, V extends Vertex> ex
     }
 
     public void load(V[] vertices, int[] indices, N[] instances) {
-        if (instances != null && instances.length > 0) {
-            this.writeInstances(instances);
-        }
-        if (vertices != null && vertices.length > 0) {
-            this.writeVertices(vertices);
-        }
-        if (indices != null && indices.length > 0) {
-            this.writeIndices(indices);
-        }
+        this.writeInstances(instances);
+        this.writeVertices(vertices);
+        this.writeIndices(indices);
         this.updateBuffers();
     }
 
--- pe-core/src/main/java/org/saar/core/renderer/r3d/ModelBuffers3DOneVbo.java
+++ pe-core/src/main/java/org/saar/core/renderer/r3d/ModelBuffers3DOneVbo.java
@@ -1,61 +1,31 @@
 package org.saar.core.renderer.r3d;
 
-import org.saar.core.model.InstancedElementsModel;
-import org.saar.core.model.Model;
-import org.saar.core.model.loader.AbstractModelBuffers;
-import org.saar.core.model.loader.ModelBuffer;
-import org.saar.lwjgl.opengl.constants.DataType;
-import org.saar.lwjgl.opengl.constants.RenderMode;
-import org.saar.lwjgl.opengl.constants.VboUsage;
-import org.saar.lwjgl.opengl.objects.DataBuffer;
-import org.saar.lwjgl.opengl.objects.IndexBuffer;
+import org.saar.core.model.loader.ModelBuffersSingleVbo;
+import org.saar.lwjgl.opengl.objects.Attribute;
 import org.saar.maths.objects.Transform;
 
-public class ModelBuffers3DOneVbo extends AbstractModelBuffers<Node3D, Vertex3D> implements ModelBuffers3D {
+public class ModelBuffers3DOneVbo extends ModelBuffersSingleVbo<Node3D, Vertex3D> implements ModelBuffers3D {
 
-    private final ModelBuffer instanceBuffer;
-    private final ModelBuffer vertexBuffer;
-    private final ModelBuffer indexBuffer;
-
-    private final Model model;
+    private static final Attribute[] vertexAttributes = {positionAttribute, coloursAttribute};
 
     public ModelBuffers3DOneVbo(int vertices, int indices, int instances) {
-        this.instanceBuffer = loadDataBuffer(new DataBuffer(VboUsage.STATIC_DRAW),
-                instances, transformAttributes);
-        this.vertexBuffer = loadDataBuffer(new DataBuffer(VboUsage.STATIC_DRAW),
-                vertices, positionAttribute, coloursAttribute);
-        this.indexBuffer = loadIndexBuffer(new IndexBuffer(VboUsage.STATIC_DRAW), indices);
-
-        this.model = new InstancedElementsModel(this.vao, RenderMode.TRIANGLES, indices, DataType.U_INT, instances);
+        super(vertices, indices, instances, vertexAttributes, transformAttributes);
     }
 
     @Override
     public void writeInstance(Node3D instance) {
         final Transform transform = instance.getTransform();
-        this.instanceBuffer.getWriter().write(transform.getTransformationMatrix());
+        instanceWriter().write(transform.getTransformationMatrix());
     }
 
     @Override
     public void writeVertex(Vertex3D vertex) {
-        this.vertexBuffer.getWriter().write(vertex.getPosition3f());
-        this.vertexBuffer.getWriter().write(vertex.getColour3f());
+        vertexWriter().write(vertex.getPosition3f());
+        vertexWriter().write(vertex.getColour3f());
     }
 
     @Override
     public void writeIndex(int index) {
-        this.indexBuffer.getWriter().write(index);
-    }
-
-    @Override
-    public void load(Vertex3D[] vertices, int[] indices, Node3D[] instances) {
-        this.writeInstances(instances);
-        this.writeVertices(vertices);
-        this.writeIndices(indices);
-        this.updateBuffers();
-    }
-
-    @Override
-    public Model getModel() {
-        return this.model;
+        indexWriter().write(index);
     }
 }
