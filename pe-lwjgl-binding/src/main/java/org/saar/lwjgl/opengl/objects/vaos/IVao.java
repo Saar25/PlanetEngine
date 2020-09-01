8d8c463aac267a3e49e54d1c051f9b8fd64fed2a
Saar <notnow@maybelater.com>
2020-09-01 12:11:27 +0300

    Moved methods to ReadOnlyVao

--- pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/objects/vaos/IVao.java
+++ pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/objects/vaos/IVao.java
@@ -3,16 +3,6 @@ package org.saar.lwjgl.opengl.objects.vaos;
 public interface IVao extends ReadOnlyVao {
 
     /**
-     * Enables the vao attributes
-     */
-    void enableAttributes();
-
-    /**
-     * Disables the vao attributes
-     */
-    void disableAttributes();
-
-    /**
      * Delete this vao and its related buffers
      */
     void delete();
--- pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/objects/vaos/ReadOnlyVao.java
+++ pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/objects/vaos/ReadOnlyVao.java
@@ -3,6 +3,16 @@ package org.saar.lwjgl.opengl.objects.vaos;
 public interface ReadOnlyVao {
 
     /**
+     * Enables the vao attributes
+     */
+    void enableAttributes();
+
+    /**
+     * Disables the vao attributes
+     */
+    void disableAttributes();
+
+    /**
      * Bind the vao
      */
     void bind();
