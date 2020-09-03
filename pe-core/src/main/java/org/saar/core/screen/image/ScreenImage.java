5a754a3b6ab3d8a10df94880e5f9aa51ad1fe350
Saar <notnow@maybelater.com>
2020-09-03 18:22:31 +0300

    Removed getTexture method

--- pe-core/src/main/java/org/saar/core/screen/image/ScreenImage.java
+++ pe-core/src/main/java/org/saar/core/screen/image/ScreenImage.java
@@ -1,13 +1,10 @@
 package org.saar.core.screen.image;
 
 import org.saar.lwjgl.opengl.fbos.ReadOnlyFbo;
-import org.saar.lwjgl.opengl.textures.ReadOnlyTexture;
 
 public interface ScreenImage {
 
     void init(ReadOnlyFbo fbo);
-
-    ReadOnlyTexture getTexture();
 
     void delete();
 }
