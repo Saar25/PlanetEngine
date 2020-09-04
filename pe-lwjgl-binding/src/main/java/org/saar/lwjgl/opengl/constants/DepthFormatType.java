cf2a84f976713d9877746437f1d48919274c5d91
Saar <notnow@maybelater.com>
2020-09-04 12:29:35 +0300

    Created DepthFormatType

---
+++ pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/constants/DepthFormatType.java
@@ -0,0 +1,22 @@
+package org.saar.lwjgl.opengl.constants;
+
+import org.lwjgl.opengl.GL14;
+
+public enum DepthFormatType {
+
+    DEPTH_COMPONENT16(GL14.GL_DEPTH_COMPONENT16),
+    DEPTH_COMPONENT24(GL14.GL_DEPTH_COMPONENT24),
+    DEPTH_COMPONENT32(GL14.GL_DEPTH_COMPONENT32),
+    ;
+
+    private final int value;
+
+    DepthFormatType(int value) {
+        this.value = value;
+    }
+
+    public int get() {
+        return this.value;
+    }
+
+}
