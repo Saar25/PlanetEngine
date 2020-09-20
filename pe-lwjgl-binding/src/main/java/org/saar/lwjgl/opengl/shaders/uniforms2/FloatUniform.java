5b18fd3c8dd09f6938f1f38297241938048b961a
Saar <notnow@maybelater.com>
2020-09-20 16:00:19 +0300

    Created Stage and instance uniform support

--- pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/shaders/uniforms2/FloatUniform.java
+++ pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/shaders/uniforms2/FloatUniform.java
@@ -1,13 +1,69 @@
 package org.saar.lwjgl.opengl.shaders.uniforms2;
 
 import org.lwjgl.opengl.GL20;
+import org.saar.lwjgl.opengl.shaders.InstanceRenderState;
+import org.saar.lwjgl.opengl.shaders.StageRenderState;
 
 public abstract class FloatUniform extends UniformBase implements Uniform {
 
     @Override
-    public void doLoad() {
+    public final void doLoad() {
         GL20.glUniform1f(getLocation(), getUniformValue());
     }
 
     public abstract float getUniformValue();
+
+    public static abstract class Stage extends FloatUniform implements StageUniform {
+
+        private final String name;
+        private float uniformValue = 0;
+
+        public Stage(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public final float getUniformValue() {
+            return this.uniformValue;
+        }
+
+        @Override
+        public final void update(StageRenderState state) {
+            this.uniformValue = getUniformValue(state);
+        }
+
+        @Override
+        public final String getName() {
+            return this.name;
+        }
+
+        public abstract float getUniformValue(StageRenderState state);
+    }
+
+    public static abstract class Instance<T> extends FloatUniform implements InstanceUniform<T> {
+
+        private final String name;
+        private float uniformValue = 0;
+
+        public Instance(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public final float getUniformValue() {
+            return this.uniformValue;
+        }
+
+        @Override
+        public void update(InstanceRenderState<T> state) {
+            this.uniformValue = getUniformValue(state);
+        }
+
+        @Override
+        public final String getName() {
+            return this.name;
+        }
+
+        public abstract float getUniformValue(InstanceRenderState<T> state);
+    }
 }
--- pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/shaders/uniforms2/IntUniform.java
+++ pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/shaders/uniforms2/IntUniform.java
@@ -1,13 +1,69 @@
 package org.saar.lwjgl.opengl.shaders.uniforms2;
 
 import org.lwjgl.opengl.GL20;
+import org.saar.lwjgl.opengl.shaders.InstanceRenderState;
+import org.saar.lwjgl.opengl.shaders.StageRenderState;
 
 public abstract class IntUniform extends UniformBase implements Uniform {
 
     @Override
-    public void doLoad() {
+    public final void doLoad() {
         GL20.glUniform1i(getLocation(), getUniformValue());
     }
 
     public abstract int getUniformValue();
+
+    public static abstract class Stage extends IntUniform implements StageUniform {
+
+        private final String name;
+        private int uniformValue = 0;
+
+        public Stage(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public final int getUniformValue() {
+            return this.uniformValue;
+        }
+
+        @Override
+        public final void update(StageRenderState state) {
+            this.uniformValue = getUniformValue(state);
+        }
+
+        @Override
+        public final String getName() {
+            return this.name;
+        }
+
+        public abstract int getUniformValue(StageRenderState state);
+    }
+
+    public static abstract class Instance<T> extends IntUniform implements InstanceUniform<T> {
+
+        private final String name;
+        private int uniformValue = 0;
+
+        public Instance(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public final int getUniformValue() {
+            return this.uniformValue;
+        }
+
+        @Override
+        public void update(InstanceRenderState<T> state) {
+            this.uniformValue = getUniformValue(state);
+        }
+
+        @Override
+        public final String getName() {
+            return this.name;
+        }
+
+        public abstract int getUniformValue(InstanceRenderState<T> state);
+    }
 }
