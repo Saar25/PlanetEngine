153d921f7ce3a45172347c14ae670286609ea999
Saar <notnow@maybelater.com>
2020-09-20 12:00:02 +0300

    Added mouse rotation

--- planet-examples/src/main/java/org/saar/example/ExamplesUtils.java
+++ planet-examples/src/main/java/org/saar/example/ExamplesUtils.java
@@ -3,7 +3,11 @@ package org.saar.example;
 import org.joml.Vector3f;
 import org.lwjgl.glfw.GLFW;
 import org.saar.core.camera.ICamera;
+import org.saar.lwjgl.glfw.input.EventListener;
 import org.saar.lwjgl.glfw.input.keyboard.Keyboard;
+import org.saar.lwjgl.glfw.input.mouse.Mouse;
+import org.saar.lwjgl.glfw.input.mouse.MouseButton;
+import org.saar.lwjgl.glfw.input.mouse.MoveEvent;
 import org.saar.maths.Angle;
 import org.saar.maths.utils.Vector3;
 
@@ -15,6 +19,10 @@ public final class ExamplesUtils {
     }
 
     public static void move(ICamera camera, Keyboard keyboard) {
+        ExamplesUtils.move(camera, keyboard, 1);
+    }
+
+    public static void move(ICamera camera, Keyboard keyboard, long ms) {
         final Vector3f toMove = Vector3.zero();
         final Vector3f toRotate = Vector3.zero();
         if (keyboard.isKeyPressed('W')) {
@@ -41,7 +49,35 @@ public final class ExamplesUtils {
         if (keyboard.isKeyPressed(GLFW.GLFW_KEY_SPACE)) {
             toMove.add(0, 1, 0);
         }
+        toMove.mul(ms / 1000f).mul(100);
+        toRotate.mul(ms / 1000f).mul(100);
         camera.getTransform().getPosition().add(toMove.rotate(camera.getTransform().getRotation().getValue()).mul(-1));
         camera.getTransform().addRotation(Angle.degrees(toRotate.x), Angle.degrees(toRotate.y), Angle.degrees(toRotate.z));
+    }
+
+    public static void addRotationListener(ICamera camera, Mouse mouse) {
+        mouse.addMoveListener(new EventListener<MoveEvent>() {
+            private long last = System.currentTimeMillis();
+            private int lastX = mouse.getXPos();
+            private int lastY = mouse.getYPos();
+
+            @Override
+            public void onEvent(MoveEvent e) {
+                final long d = System.currentTimeMillis() - last;
+                if (d < 300 && mouse.isButtonDown(MouseButton.PRIMARY)) {
+                    final Vector3f toRotate = Vector3.zero();
+                    toRotate.y += this.lastX - e.getX();
+                    toRotate.x += this.lastY - e.getY();
+                    toRotate.mul(d / 1000f).mul(30);
+                    camera.getTransform().addRotation(
+                            Angle.degrees(toRotate.x),
+                            Angle.degrees(toRotate.y),
+                            Angle.degrees(toRotate.z));
+                }
+                lastX = (int) e.getX();
+                lastY = (int) e.getY();
+                last = System.currentTimeMillis();
+            }
+        });
     }
 }
--- planet-examples/src/main/java/org/saar/example/deferred/DeferredExample.java
+++ planet-examples/src/main/java/org/saar/example/deferred/DeferredExample.java
@@ -10,6 +10,7 @@ import org.saar.core.renderer.deferred.DeferredRenderingPath;
 import org.saar.core.renderer.deferred.light.LightRenderPass;
 import org.saar.example.ExamplesUtils;
 import org.saar.lwjgl.glfw.input.keyboard.Keyboard;
+import org.saar.lwjgl.glfw.input.mouse.Mouse;
 import org.saar.lwjgl.glfw.window.Window;
 import org.saar.lwjgl.opengl.textures.Texture2D;
 import org.saar.maths.transform.Position;
@@ -20,7 +21,7 @@ public class DeferredExample {
     private static final int HEIGHT = 500;
 
     public static void main(String[] args) {
-        final Window window = new Window("Lwjgl", WIDTH, HEIGHT, true);
+        final Window window = new Window("Lwjgl", WIDTH, HEIGHT, false);
         window.init();
 
         final PerspectiveProjection projection = new PerspectiveProjection(70f, WIDTH, HEIGHT, 1, 1000);
@@ -51,15 +52,24 @@ public class DeferredExample {
         deferredRenderer.addRenderer(renderer);
         deferredRenderer.addRenderPass(new LightRenderPass(camera));
 
+        final Mouse mouse = window.getMouse();
+        ExamplesUtils.addRotationListener(camera, mouse);
+
+        long current = System.currentTimeMillis();
         final Keyboard keyboard = window.getKeyboard();
         while (window.isOpen() && !keyboard.isKeyPressed('T')) {
-
-            ExamplesUtils.move(camera, keyboard);
 
             deferredRenderer.render();
 
             window.update(true);
             window.pollEvents();
+
+            final long delta = System.currentTimeMillis() - current;
+            ExamplesUtils.move(camera, keyboard, delta);
+
+            System.out.print("\rFps: " +
+                    1000f / (-current + (current = System.currentTimeMillis()))
+            );
         }
 
         renderer.delete();
