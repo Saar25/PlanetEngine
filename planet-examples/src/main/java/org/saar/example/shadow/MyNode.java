f11a98b2ca67e7aae4549334139cda5c5b99a549
Saar <notnow@maybelater.com>
2020-09-25 18:24:28 +0300

    Created ShadowExample

---
+++ planet-examples/src/main/java/org/saar/example/shadow/MyNode.java
@@ -0,0 +1,27 @@
+package org.saar.example.shadow;
+
+import org.saar.core.common.obj.ObjNode;
+import org.saar.core.node.AbstractNode;
+import org.saar.lwjgl.opengl.textures.ReadOnlyTexture;
+import org.saar.maths.transform.SimpleTransform;
+
+public class MyNode extends AbstractNode implements ObjNode {
+
+    private final SimpleTransform transform = new SimpleTransform();
+
+    private final ReadOnlyTexture texture;
+
+    public MyNode(ReadOnlyTexture texture) {
+        this.texture = texture;
+    }
+
+    @Override
+    public SimpleTransform getTransform() {
+        return this.transform;
+    }
+
+    @Override
+    public ReadOnlyTexture getTexture() {
+        return this.texture;
+    }
+}
