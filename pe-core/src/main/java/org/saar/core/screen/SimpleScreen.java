5df9bafcac78d4deab91d689f7ca0561daf2b8ed
Saar <notnow@maybelater.com>
2020-09-26 18:13:27 +0300

    Added setDrawImages and setReadImage

--- pe-core/src/main/java/org/saar/core/screen/SimpleScreen.java
+++ pe-core/src/main/java/org/saar/core/screen/SimpleScreen.java
@@ -1,7 +1,9 @@
 package org.saar.core.screen;
 
+import org.saar.core.screen.image.ColourScreenImage;
 import org.saar.core.screen.image.ScreenImage;
 import org.saar.lwjgl.opengl.fbos.IFbo;
+import org.saar.lwjgl.opengl.fbos.attachment.Attachment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +21,15 @@ public class SimpleScreen extends ScreenBase implements OffScreen {
     public void addScreenImage(ScreenImage image) {
         image.getAttachment().init(this.fbo);
         getScreenImages().add(image);
+    }
+
+    public void setDrawImages(ScreenImage... images) {
+        final Attachment[] attachment = Screens.toAttachments(images);
+        getFbo().setDrawAttachments(attachment);
+    }
+
+    public void setReadImages(ColourScreenImage image) {
+        getFbo().setReadAttachment(image.getAttachment());
     }
 
     @Override
