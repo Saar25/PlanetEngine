df166ea1094cbc867df07729510454db57131380
Saar <notnow@maybelater.com>
2020-09-26 12:43:20 +0300

    Created ShadowsCamera

---
+++ pe-core/src/main/java/org/saar/core/renderer/deferred/shadow/ShadowsCamera.java
@@ -0,0 +1,39 @@
+package org.saar.core.renderer.deferred.shadow;
+
+import org.joml.Matrix4f;
+import org.joml.Matrix4fc;
+import org.saar.core.camera.ICamera;
+import org.saar.core.camera.projection.OrthographicProjection;
+import org.saar.maths.transform.SimpleTransform;
+import org.saar.maths.transform.Transform;
+import org.saar.maths.utils.Matrix4;
+
+public class ShadowsCamera implements ICamera {
+
+
+    private final Matrix4f viewMatrix = Matrix4.create();
+    private final Transform transform = new SimpleTransform();
+    private final OrthographicProjection projection;
+
+    public ShadowsCamera(OrthographicProjection projection) {
+        this.projection = projection;
+    }
+
+    @Override
+    public Matrix4fc getViewMatrix() {
+        return Matrix4.ofView(
+                getTransform().getPosition().getValue(),
+                getTransform().getRotation().getValue(),
+                this.viewMatrix);
+    }
+
+    @Override
+    public Transform getTransform() {
+        return this.transform;
+    }
+
+    @Override
+    public OrthographicProjection getProjection() {
+        return this.projection;
+    }
+}
