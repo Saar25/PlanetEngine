ff6f965d131b7bcec2318f7a381466989a6acd44
Saar <notnow@maybelater.com>
2020-09-27 15:39:22 +0300

    Added models

--- planet-examples/src/main/java/org/saar/example/shadow/ShadowExample.java
+++ planet-examples/src/main/java/org/saar/example/shadow/ShadowExample.java
@@ -3,9 +3,7 @@ package org.saar.example.shadow;
 import org.saar.core.camera.Camera;
 import org.saar.core.camera.projection.OrthographicProjection;
 import org.saar.core.camera.projection.PerspectiveProjection;
-import org.saar.core.common.obj.ObjDeferredRenderer;
-import org.saar.core.common.obj.ObjMesh;
-import org.saar.core.common.obj.ObjRenderNode;
+import org.saar.core.common.obj.*;
 import org.saar.core.common.r3d.*;
 import org.saar.core.light.DirectionalLight;
 import org.saar.core.renderer.deferred.DeferredRenderingPath;
@@ -16,8 +14,13 @@ import org.saar.example.ExamplesUtils;
 import org.saar.lwjgl.glfw.input.keyboard.Keyboard;
 import org.saar.lwjgl.glfw.input.mouse.Mouse;
 import org.saar.lwjgl.glfw.window.Window;
+import org.saar.lwjgl.opengl.textures.ColourTexture;
+import org.saar.lwjgl.opengl.textures.ReadOnlyTexture;
 import org.saar.lwjgl.opengl.textures.Texture2D;
+import org.saar.maths.Angle;
 import org.saar.maths.transform.Position;
+
+import java.util.Objects;
 
 public class ShadowExample {
 
@@ -34,21 +37,16 @@ public class ShadowExample {
         camera.getTransform().getPosition().set(0, 0, 200);
         camera.getTransform().lookAt(Position.of(0, 0, 0));
 
-        MyNode node;
-        ObjRenderNode renderNode = null;
-        Texture2D texture;
-        try {
-            final ObjMesh mesh = ObjMesh.load("/assets/cottage/cottage.obj");
-            texture = Texture2D.of("/assets/cottage/cottage_diffuse.png");
-            node = new MyNode(texture);
+        final ObjRenderNode cottageNode = Objects.requireNonNull(loadCottage());
 
-            renderNode = new ObjRenderNode(mesh, node);
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.exit(1);
-        }
+        final ObjRenderNode dragonNode = Objects.requireNonNull(loadDragon());
+        dragonNode.getTransform().getPosition().set(50, 0, 0);
 
-        final ObjDeferredRenderer renderer = new ObjDeferredRenderer(renderNode);
+        final ObjRenderNode stallNode = Objects.requireNonNull(loadStall());
+        stallNode.getTransform().getPosition().set(-50, 0, 0);
+        stallNode.getTransform().getRotation().rotate(Angle.degrees(0), Angle.degrees(180), Angle.degrees(0));
+
+        final ObjDeferredRenderer renderer = new ObjDeferredRenderer(cottageNode, dragonNode, stallNode);
 
         final Node3D cube = new Spatial3D();
         cube.getTransform().getScale().set(10, 10, 10);
@@ -69,7 +67,7 @@ public class ShadowExample {
         final OrthographicProjection shadowProjection = new OrthographicProjection(
                 -100, 100, -100, 100, -100, 100);
         final ShadowsRenderingPath shadowsRenderingPath = new ShadowsRenderingPath(
-                ShadowsQuality.LOW, shadowProjection, light);
+                ShadowsQuality.VERY_HIGH, shadowProjection, light);
         shadowsRenderingPath.addRenderer(renderer3D);
         shadowsRenderingPath.addRenderer(renderer);
 
@@ -105,6 +103,42 @@ public class ShadowExample {
         shadowsRenderingPath.delete();
         deferredRenderer.delete();
         window.destroy();
+    }
+
+    private static ObjRenderNode loadCottage() {
+        try {
+            final ObjMesh mesh = ObjMesh.load("/assets/cottage/cottage.obj");
+            final Texture2D texture = Texture2D.of("/assets/cottage/cottage_diffuse.png");
+            final ObjNode node = new ObjSpatial(texture);
+            return new ObjRenderNode(mesh, node);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static ObjRenderNode loadStall() {
+        try {
+            final ObjMesh mesh = ObjMesh.load("/assets/stall/stall.model.obj");
+            final Texture2D texture = Texture2D.of("/assets/stall/stall.diffuse.png");
+            final ObjNode node = new ObjSpatial(texture);
+            return new ObjRenderNode(mesh, node);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static ObjRenderNode loadDragon() {
+        try {
+            final ObjMesh mesh = ObjMesh.load("/assets/dragon/dragon.model.obj");
+            final ReadOnlyTexture texture = ColourTexture.of(255, 215, 0, 255);
+            final ObjNode node = new ObjSpatial(texture);
+            return new ObjRenderNode(mesh, node);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
