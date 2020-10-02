12da80423ef5a8c7b9a4efcf92e6b4809c86140f
Saar <notnow@maybelater.com>
2020-10-02 20:43:51 +0300

    Removed unused files

--- pe-core/src/main/java/org/saar/core/loader/Loaders.java
+++
@@ -1,9 +0,0 @@
-package org.saar.core.loader;
-
-public final class Loaders {
-
-    private Loaders() {
-        throw new AssertionError("Cannot create instance of class "
-                + getClass().getSimpleName());
-    }
-}
