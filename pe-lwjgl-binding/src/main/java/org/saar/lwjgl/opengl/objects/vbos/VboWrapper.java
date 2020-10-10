4776259d58955b000f150bf58291f89b6db9dd71
Saar <notnow@maybelater.com>
2020-10-10 17:40:53 +0300

    Create VboWrapper

---
+++ pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/objects/vbos/VboWrapper.java
@@ -0,0 +1,37 @@
+package org.saar.lwjgl.opengl.objects.vbos;
+
+import org.lwjgl.system.MemoryUtil;
+import org.saar.lwjgl.opengl.utils.BufferWriter;
+
+import java.nio.ByteBuffer;
+
+public class VboWrapper {
+
+    private final IVbo vbo;
+
+    private ByteBuffer buffer = null;
+    private BufferWriter writer = null;
+
+    public VboWrapper(IVbo vbo) {
+        this.vbo = vbo;
+    }
+
+    public void allocate(int capacity) {
+        MemoryUtil.memFree(this.buffer);
+        this.vbo.allocateByte(capacity);
+        this.buffer = MemoryUtil.memAlloc(capacity);
+        this.writer = new BufferWriter(this.buffer);
+    }
+
+    public void store(long offset) {
+        this.buffer.flip();
+        this.vbo.storeData(offset, this.buffer);
+        MemoryUtil.memFree(this.buffer);
+        this.buffer = null;
+        this.writer = null;
+    }
+
+    public BufferWriter getWriter() {
+        return this.writer;
+    }
+}
