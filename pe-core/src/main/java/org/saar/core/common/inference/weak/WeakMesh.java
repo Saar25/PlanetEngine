55e2645c0f8874d451f6f02081131aeee0131198
Saar <notnow@maybelater.com>
2020-10-11 22:16:25 +0300

    Refactored WeakInference mesh building

--- pe-core/src/main/java/org/saar/core/common/inference/weak/WeakMesh.java
+++ pe-core/src/main/java/org/saar/core/common/inference/weak/WeakMesh.java
@@ -1,13 +1,17 @@
 package org.saar.core.common.inference.weak;
 
 import org.saar.core.model.*;
-import org.saar.core.model.mesh.MeshPrototypeHelper;
 import org.saar.core.model.mesh.MeshWriters;
 import org.saar.lwjgl.opengl.constants.DataType;
 import org.saar.lwjgl.opengl.constants.RenderMode;
 import org.saar.lwjgl.opengl.objects.Attribute;
 import org.saar.lwjgl.opengl.objects.vaos.Vao;
-import org.saar.lwjgl.opengl.utils.BufferWriter;
+import org.saar.lwjgl.opengl.objects.vbos.Vbo;
+import org.saar.lwjgl.opengl.objects.vbos.VboTarget;
+import org.saar.lwjgl.opengl.objects.vbos.VboUsage;
+import org.saar.lwjgl.opengl.objects.vbos.VboWrapper;
+
+import java.util.Arrays;
 
 public class WeakMesh implements Mesh {
 
@@ -18,123 +22,107 @@ public class WeakMesh implements Mesh {
     }
 
     public static WeakMesh load(WeakVertex[] vertices, int[] indices, WeakInstance[] instances) {
-        final InstancedElementsWeakMesh prototype = new InstancedElementsWeakMesh();
-        final MeshPrototypeHelper helper = new MeshPrototypeHelper(prototype);
         int vertexAttributes = 0;
+
+        final Vao vao = Vao.create();
 
         if (vertices.length > 0) {
             final Attribute[] attributes = vertices[0].getAttributes();
-            prototype.getMeshVertexBuffer().addAttributes(attributes);
             vertexAttributes = attributes.length;
-
-            helper.allocateVertices(vertices);
-            final BufferWriter vertexWriter = prototype.getMeshVertexBuffer().getWriter();
-            MeshWriters.writeVertices((vertex -> vertex.write(vertexWriter)), vertices);
+            loadVertices(vao, vertices, attributes);
         }
 
         if (instances.length > 0) {
             final Attribute[] attributes = instances[0].getAttributes(vertexAttributes);
-            prototype.getMeshInstanceBuffer().addAttributes(attributes);
-
-            helper.allocateInstances(instances);
-            final BufferWriter instanceWriter = prototype.getMeshInstanceBuffer().getWriter();
-            MeshWriters.writeNodes((instance -> instance.write(instanceWriter)), instances);
+            loadInstances(vao, instances, attributes);
         }
 
-        helper.allocateIndices(indices);
-        final BufferWriter indexWriter = prototype.getMeshIndexBuffer().getWriter();
-        MeshWriters.writeIndices(indexWriter::write, indices);
+        loadIndices(vao, indices);
 
-        helper.store();
-
-        final Vao vao = Vao.create();
-        helper.loadToVao(vao);
-
-        final Mesh mesh = new InstancedElementsMesh(vao,
-                RenderMode.TRIANGLES, indices.length, DataType.U_INT, instances.length);
+        final Mesh mesh = new InstancedElementsMesh(vao, RenderMode.TRIANGLES,
+                indices.length, DataType.U_INT, instances.length);
         return new WeakMesh(mesh);
     }
 
     public static WeakMesh load(WeakVertex[] vertices, WeakInstance[] instances) {
-        final InstancedArraysWeakMesh prototype = new InstancedArraysWeakMesh();
-        final MeshPrototypeHelper helper = new MeshPrototypeHelper(prototype);
         int vertexAttributes = 0;
+
+        final Vao vao = Vao.create();
 
         if (vertices.length > 0) {
             final Attribute[] attributes = vertices[0].getAttributes();
-            prototype.getMeshVertexBuffer().addAttributes(attributes);
             vertexAttributes = attributes.length;
-
-            helper.allocateVertices(vertices);
-            final BufferWriter vertexWriter = prototype.getMeshVertexBuffer().getWriter();
-            MeshWriters.writeVertices((vertex -> vertex.write(vertexWriter)), vertices);
+            loadVertices(vao, vertices, attributes);
         }
 
         if (instances.length > 0) {
             final Attribute[] attributes = instances[0].getAttributes(vertexAttributes);
-            prototype.getMeshInstanceBuffer().addAttributes(attributes);
-
-            helper.allocateInstances(instances);
-            final BufferWriter instanceWriter = prototype.getMeshInstanceBuffer().getWriter();
-            MeshWriters.writeNodes((instance -> instance.write(instanceWriter)), instances);
+            loadInstances(vao, instances, attributes);
         }
 
-        helper.store();
-
-        final Vao vao = Vao.create();
-        helper.loadToVao(vao);
-
-        final Mesh mesh = new InstancedArraysMesh(vao,
-                RenderMode.TRIANGLES, vertices.length, instances.length);
+        final Mesh mesh = new InstancedArraysMesh(vao, RenderMode.TRIANGLES,
+                vertices.length, instances.length);
         return new WeakMesh(mesh);
     }
 
     public static WeakMesh load(WeakVertex[] vertices, int[] indices) {
-        final ElementsWeakMesh prototype = new ElementsWeakMesh();
-        final MeshPrototypeHelper helper = new MeshPrototypeHelper(prototype);
+        final Vao vao = Vao.create();
 
         if (vertices.length > 0) {
-            prototype.getMeshVertexBuffer().addAttributes(vertices[0].getAttributes());
-
-            helper.allocateVertices(vertices);
-            final BufferWriter vertexWriter = prototype.getMeshVertexBuffer().getWriter();
-            MeshWriters.writeVertices((vertex -> vertex.write(vertexWriter)), vertices);
+            final Attribute[] attributes = vertices[0].getAttributes();
+            loadVertices(vao, vertices, attributes);
         }
 
-        helper.allocateIndices(indices);
-        final BufferWriter indexWriter = prototype.getMeshIndexBuffer().getWriter();
-        MeshWriters.writeIndices(indexWriter::write, indices);
+        loadIndices(vao, indices);
 
-        helper.store();
-
-        final Vao vao = Vao.create();
-        helper.loadToVao(vao);
-
-        final Mesh mesh = new ElementsMesh(vao,
-                RenderMode.TRIANGLES, indices.length, DataType.U_INT);
+        final Mesh mesh = new ElementsMesh(vao, RenderMode.TRIANGLES,
+                indices.length, DataType.U_INT);
         return new WeakMesh(mesh);
     }
 
     public static WeakMesh load(WeakVertex[] vertices) {
-        final ArraysWeakMesh prototype = new ArraysWeakMesh();
-        final MeshPrototypeHelper helper = new MeshPrototypeHelper(prototype);
+        final Vao vao = Vao.create();
 
         if (vertices.length > 0) {
-            prototype.getMeshVertexBuffer().addAttributes(vertices[0].getAttributes());
-
-            helper.allocateVertices(vertices);
-            final BufferWriter vertexWriter = prototype.getMeshVertexBuffer().getWriter();
-            MeshWriters.writeVertices((vertex -> vertex.write(vertexWriter)), vertices);
+            final Attribute[] attributes = vertices[0].getAttributes();
+            loadVertices(vao, vertices, attributes);
         }
-
-        helper.store();
-
-        final Vao vao = Vao.create();
-        helper.loadToVao(vao);
 
         final Mesh mesh = new ArraysMesh(vao,
                 RenderMode.TRIANGLES, vertices.length);
         return new WeakMesh(mesh);
+    }
+
+    private static void loadIndices(Vao vao, int[] indices) {
+        final Vbo vbo = Vbo.create(VboTarget.ELEMENT_ARRAY_BUFFER, VboUsage.STATIC_DRAW);
+        final VboWrapper wrapper = new VboWrapper(vbo);
+
+        wrapper.allocate(indices.length * DataType.INT.getBytes());
+        MeshWriters.writeIndices(wrapper.getWriter()::write, indices);
+        wrapper.store(0);
+        vao.loadVbo(vbo);
+    }
+
+    private static void loadVertices(Vao vao, WeakVertex[] vertices, Attribute[] attributes) {
+        final Vbo vbo = Vbo.create(VboTarget.ARRAY_BUFFER, VboUsage.STATIC_DRAW);
+        final VboWrapper wrapper = new VboWrapper(vbo);
+
+        wrapper.allocate(vertices.length * Attribute.sumBytes(attributes));
+        MeshWriters.writeVertices(vertex -> vertex.write(wrapper.getWriter()), vertices);
+        wrapper.store(0);
+
+        vao.loadVbo(vbo, attributes);
+    }
+
+    private static void loadInstances(Vao vao, WeakInstance[] instances, Attribute[] attributes) {
+        final Vbo vbo = Vbo.create(VboTarget.ARRAY_BUFFER, VboUsage.STATIC_DRAW);
+        final VboWrapper wrapper = new VboWrapper(vbo);
+
+        wrapper.allocate(instances.length * Attribute.sumBytes(attributes));
+        MeshWriters.writeNodes(instance -> instance.write(wrapper.getWriter()), instances);
+        wrapper.store(0);
+
+        vao.loadVbo(vbo, attributes);
     }
 
     @Override
