01eb45861d82e41d60d362079a47f1c97286a2af
Saar <notnow@maybelater.com>
2020-10-27 18:27:31 +0200

    Created UnloadedMesh

--- pe-core/src/main/java/org/saar/core/model/FutureMeshHelper.java
+++ pe-core/src/main/java/org/saar/core/model/FutureMeshHelper.java
@@ -16,6 +16,10 @@ abstract class FutureMeshHelper implements Mesh {
         return new Running(task);
     }
 
+    public static FutureMeshHelper unloaded(CompletableFuture<UnloadedMesh> task) {
+        return new Unloaded(task);
+    }
+
     public static class Running extends FutureMeshHelper {
 
         private final CompletableFuture<Mesh> task;
@@ -28,6 +32,34 @@ abstract class FutureMeshHelper implements Mesh {
         public FutureMeshHelper next() {
             if (this.task.isDone()) {
                 final Mesh mesh = this.task.getNow(null);
+                return new Finished(mesh);
+            }
+            return this;
+        }
+
+        @Override
+        public void draw() {
+        }
+
+        @Override
+        public void delete() {
+            this.task.cancel(true);
+        }
+    }
+
+    public static class Unloaded extends FutureMeshHelper {
+
+        private final CompletableFuture<UnloadedMesh> task;
+
+        public Unloaded(CompletableFuture<UnloadedMesh> task) {
+            this.task = task;
+        }
+
+        @Override
+        public FutureMeshHelper next() {
+            if (this.task.isDone()) {
+                final UnloadedMesh unloaded = this.task.getNow(null);
+                final Mesh mesh = unloaded.load();
                 return new Finished(mesh);
             }
             return this;
---
+++ pe-core/src/main/java/org/saar/core/model/UnloadedMesh.java
@@ -0,0 +1,7 @@
+package org.saar.core.model;
+
+public interface UnloadedMesh {
+
+    Mesh load();
+
+}
