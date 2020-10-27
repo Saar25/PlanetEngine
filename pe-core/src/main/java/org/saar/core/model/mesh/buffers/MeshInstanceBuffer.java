f7b1fe2c5413ca38b979a096ee0a4940ea2c83f1
Saar <notnow@maybelater.com>
2020-10-27 17:12:59 +0200

    Created MeshDataBuffer to prevent code repetition

--- pe-core/src/main/java/org/saar/core/model/mesh/buffers/MeshInstanceBuffer.java
+++ pe-core/src/main/java/org/saar/core/model/mesh/buffers/MeshInstanceBuffer.java
@@ -1,23 +1,15 @@
 package org.saar.core.model.mesh.buffers;
 
-import org.saar.core.model.mesh.MeshBuffer;
-import org.saar.lwjgl.opengl.objects.Attribute;
 import org.saar.lwjgl.opengl.objects.buffers.BufferObjectWrapper;
-import org.saar.lwjgl.opengl.objects.vaos.WriteableVao;
-import org.saar.lwjgl.opengl.objects.vbos.*;
+import org.saar.lwjgl.opengl.objects.vbos.IVbo;
+import org.saar.lwjgl.opengl.objects.vbos.Vbo;
+import org.saar.lwjgl.opengl.objects.vbos.VboTarget;
+import org.saar.lwjgl.opengl.objects.vbos.VboUsage;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-public class MeshInstanceBuffer extends MeshBuffer {
-
-    private final List<Attribute> attributes = new ArrayList<>();
-    private final IVbo vbo;
+public class MeshInstanceBuffer extends MeshDataBuffer {
 
     public MeshInstanceBuffer(IVbo vbo, BufferObjectWrapper wrapper) {
-        super(wrapper);
-        this.vbo = vbo;
+        super(vbo, wrapper);
     }
 
     private static MeshInstanceBuffer create(VboUsage usage) {
@@ -31,28 +23,5 @@ public class MeshInstanceBuffer extends MeshBuffer {
 
     public static MeshInstanceBuffer createDynamic() {
         return MeshInstanceBuffer.create(VboUsage.DYNAMIC_DRAW);
-    }
-
-    public void allocateCount(int count) {
-        final int bytes = Attribute.sumBytes(attributesArray());
-        final int capacity = count * bytes;
-        allocate(capacity);
-    }
-
-    @Override
-    public void loadInVao(WriteableVao vao) {
-        vao.loadVbo(this.vbo, attributesArray());
-    }
-
-    public void addAttribute(Attribute attribute) {
-        this.attributes.add(attribute);
-    }
-
-    public void addAttributes(Attribute... attributes) {
-        Collections.addAll(this.attributes, attributes);
-    }
-
-    private Attribute[] attributesArray() {
-        return this.attributes.toArray(new Attribute[0]);
     }
 }
--- pe-core/src/main/java/org/saar/core/model/mesh/buffers/MeshVertexBuffer.java
+++ pe-core/src/main/java/org/saar/core/model/mesh/buffers/MeshVertexBuffer.java
@@ -1,23 +1,15 @@
 package org.saar.core.model.mesh.buffers;
 
-import org.saar.core.model.mesh.MeshBuffer;
-import org.saar.lwjgl.opengl.objects.Attribute;
 import org.saar.lwjgl.opengl.objects.buffers.BufferObjectWrapper;
-import org.saar.lwjgl.opengl.objects.vaos.WriteableVao;
-import org.saar.lwjgl.opengl.objects.vbos.*;
+import org.saar.lwjgl.opengl.objects.vbos.IVbo;
+import org.saar.lwjgl.opengl.objects.vbos.Vbo;
+import org.saar.lwjgl.opengl.objects.vbos.VboTarget;
+import org.saar.lwjgl.opengl.objects.vbos.VboUsage;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-public class MeshVertexBuffer extends MeshBuffer {
-
-    private final List<Attribute> attributes = new ArrayList<>();
-    private final IVbo vbo;
+public class MeshVertexBuffer extends MeshDataBuffer {
 
     public MeshVertexBuffer(IVbo vbo, BufferObjectWrapper wrapper) {
-        super(wrapper);
-        this.vbo = vbo;
+        super(vbo, wrapper);
     }
 
     private static MeshVertexBuffer create(VboUsage usage) {
@@ -31,28 +23,5 @@ public class MeshVertexBuffer extends MeshBuffer {
 
     public static MeshVertexBuffer createDynamic() {
         return MeshVertexBuffer.create(VboUsage.DYNAMIC_DRAW);
-    }
-
-    public void allocateCount(int count) {
-        final int bytes = Attribute.sumBytes(attributesArray());
-        final int capacity = count * bytes;
-        allocate(capacity);
-    }
-
-    @Override
-    public void loadInVao(WriteableVao vao) {
-        vao.loadVbo(this.vbo, attributesArray());
-    }
-
-    public void addAttribute(Attribute attribute) {
-        this.attributes.add(attribute);
-    }
-
-    public void addAttributes(Attribute... attributes) {
-        Collections.addAll(this.attributes, attributes);
-    }
-
-    private Attribute[] attributesArray() {
-        return this.attributes.toArray(new Attribute[0]);
     }
 }
