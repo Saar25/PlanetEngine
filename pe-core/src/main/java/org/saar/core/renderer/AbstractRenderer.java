6a12f91ae625991087eea845f9bdf0ed7e4d0dbb
Saar Tako <saartako25@gmail.com>
2021-03-27 13:03:55 +0300

    created method buildShadersProgram

--- pe-core/src/main/java/org/saar/core/renderer/AbstractRenderer.java
+++ pe-core/src/main/java/org/saar/core/renderer/AbstractRenderer.java
@@ -1,14 +1,32 @@
 package org.saar.core.renderer;
 
+import org.saar.core.renderer.shaders.ShadersHelper;
+import org.saar.lwjgl.opengl.shaders.Shader;
+import org.saar.lwjgl.opengl.shaders.ShaderCompileException;
 import org.saar.lwjgl.opengl.shaders.ShadersProgram;
 import org.saar.lwjgl.opengl.shaders.uniforms.Uniform;
 
 public abstract class AbstractRenderer implements Renderer {
 
-    private final ShadersProgram shadersProgram;
+    private ShadersProgram shadersProgram;
 
     public AbstractRenderer(ShadersProgram shadersProgram) {
         this.shadersProgram = shadersProgram;
+    }
+
+    public AbstractRenderer() {
+    }
+
+    protected void buildShadersProgram() throws ShaderCompileException {
+        ShadersHelper helper = ShadersHelper.empty();
+        for (Shader shader : Renderers.findVertexShaders(this)) {
+            helper = helper.addShader(shader);
+        }
+        for (Shader shader : Renderers.findFragmentShaders(this)) {
+            helper = helper.addShader(shader);
+        }
+
+        this.shadersProgram = helper.createProgram();
     }
 
     protected final void init() {
