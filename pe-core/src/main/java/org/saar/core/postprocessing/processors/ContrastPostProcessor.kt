543341673e7a4a73cdd5b4bd6aec83458d90174a
Saar Tako <saartako25@gmail.com>
2021-04-10 14:56:34 +0300

    created GaussianBlurPostProcessor

--- pe-core/src/main/java/org/saar/core/postprocessing/processors/ContrastPostProcessor.kt
+++ pe-core/src/main/java/org/saar/core/postprocessing/processors/ContrastPostProcessor.kt
@@ -33,8 +33,6 @@ private class ContrastPostProcessorPrototype(private val contrast: Float) : Post
 
     override fun onRender(context: PostProcessingContext) {
         this.textureUniform.value = context.texture
-        this.textureUniform.load()
-        this.contrastUniform.load()
     }
 
     override fun onDelete() {
---
+++ pe-core/src/main/java/org/saar/core/postprocessing/processors/GaussianBlurPostProcessor.kt
@@ -0,0 +1,63 @@
+package org.saar.core.postprocessing.processors
+
+import org.joml.Vector2i
+import org.saar.core.postprocessing.PostProcessingContext
+import org.saar.core.postprocessing.PostProcessor
+import org.saar.core.postprocessing.PostProcessorPrototype
+import org.saar.core.postprocessing.PostProcessorPrototypeWrapper
+import org.saar.core.renderer.uniforms.UniformProperty
+import org.saar.core.screen.MainScreen
+import org.saar.lwjgl.opengl.shaders.GlslVersion
+import org.saar.lwjgl.opengl.shaders.Shader
+import org.saar.lwjgl.opengl.shaders.ShaderCode
+import org.saar.lwjgl.opengl.shaders.uniforms.*
+
+private val blurLevels: Array<Float> = arrayOf(
+    0.0093f, 0.028002f, 0.065984f, 0.121703f,
+    0.175713f, 0.198596f, 0.175713f, 0.121703f,
+    0.065984f, 0.028002f, 0.0093f)
+
+class GaussianBlurPostProcessor(contrast: Float) : PostProcessor,
+    PostProcessorPrototypeWrapper(GaussianBlurPostProcessorPrototype(contrast)) {
+
+}
+
+private class GaussianBlurPostProcessorPrototype(private val contrast: Float) : PostProcessorPrototype {
+
+    @UniformProperty
+    private val textureUniform = TextureUniformValue("u_texture", 0)
+
+    @UniformProperty
+    private val resolutionUniform = object : Vec2iUniform() {
+        override fun getName() = "u_resolution"
+
+        override fun getUniformValue() = Vector2i(
+            MainScreen.getInstance().width,
+            MainScreen.getInstance().height)
+    }
+
+    @UniformProperty
+    private val blurLevelsUniform = UniformArray("u_blurLevels", blurLevels.size) { name, index ->
+        object : FloatUniform() {
+            override fun getName() = name
+
+            override fun getUniformValue() = blurLevels[index]
+        }
+    }
+
+    @UniformProperty
+    private val verticalBlurUniform = IntUniformValue("u_verticalBlur")
+
+    override fun fragmentShader(): Shader = Shader.createFragment(GlslVersion.V400,
+        ShaderCode.define("LEVELS", blurLevels.size.toString()),
+        ShaderCode.loadSource("/shaders/postprocessing/gaussian-blur.pass.glsl"))
+
+    override fun onRender(context: PostProcessingContext) {
+        this.textureUniform.value = context.texture
+    }
+
+    fun onRender(context: PostProcessingContext, vertical: Boolean) {
+        this.textureUniform.value = context.texture
+        this.verticalBlurUniform.value = if (vertical) 1 else 0
+    }
+}
