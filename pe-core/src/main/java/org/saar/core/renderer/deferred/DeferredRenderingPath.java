2c70a0a1f53000b514377e9fb0a975cf29eeeb53
Saar Tako <saartako25@gmail.com>
2021-04-11 18:40:56 +0300

    moved screen handling to DeferredRenderingPipeline

--- pe-core/src/main/java/org/saar/core/renderer/deferred/DeferredRenderingPath.java
+++ pe-core/src/main/java/org/saar/core/renderer/deferred/DeferredRenderingPath.java
@@ -2,64 +2,26 @@ package org.saar.core.renderer.deferred;
 
 import org.saar.core.camera.ICamera;
 import org.saar.core.renderer.RenderingPath;
-import org.saar.core.screen.MainScreen;
-import org.saar.core.screen.OffScreen;
-import org.saar.core.screen.SimpleScreen;
-import org.saar.core.screen.image.ColourScreenImage;
-import org.saar.lwjgl.opengl.constants.ColourFormatType;
-import org.saar.lwjgl.opengl.constants.DataType;
-import org.saar.lwjgl.opengl.constants.FormatType;
-import org.saar.lwjgl.opengl.fbos.Fbo;
-import org.saar.lwjgl.opengl.fbos.attachment.ColourAttachment;
-import org.saar.lwjgl.opengl.textures.Texture;
-import org.saar.lwjgl.opengl.utils.GlBuffer;
-import org.saar.lwjgl.opengl.utils.GlUtils;
 
 public class DeferredRenderingPath implements RenderingPath {
 
     private final ICamera camera;
     private final DeferredRenderingPipeline pipeline;
     private final DeferredRenderingBuffers buffers;
-    private final OffScreen screen;
-
-    private final Texture colourTexture = Texture.create();
 
     public DeferredRenderingPath(ICamera camera, DeferredRenderingBuffers buffers, DeferredRenderingPipeline pipeline) {
         this.camera = camera;
         this.pipeline = pipeline;
-        this.screen = screen(this.colourTexture);
         this.buffers = buffers;
-    }
-
-    private static OffScreen screen(Texture colourTexture) {
-        final SimpleScreen screen = new SimpleScreen(fbo());
-        final ColourScreenImage screenImage = new ColourScreenImage(ColourAttachment.withTexture(
-                0, colourTexture, ColourFormatType.RGB16, FormatType.RGB, DataType.U_BYTE));
-        screen.addScreenImage(screenImage);
-        screen.setReadImages(screenImage);
-        screen.setDrawImages(screenImage);
-        return screen;
-    }
-
-    private static Fbo fbo() {
-        final int width = MainScreen.getInstance().getWidth();
-        final int height = MainScreen.getInstance().getHeight();
-        return Fbo.create(width, height);
     }
 
     @Override
     public DeferredRenderingOutput render() {
-        this.screen.setAsDraw();
-        GlUtils.clear(GlBuffer.COLOUR);
-
-        this.pipeline.render(this.camera, this.colourTexture, this.buffers);
-
-        return new DeferredRenderingOutput(this.screen, this.colourTexture);
+        return this.pipeline.process(this.camera, this.buffers);
     }
 
     @Override
     public void delete() {
-        this.screen.delete();
         this.pipeline.delete();
     }
 }
--- pe-core/src/main/java/org/saar/core/renderer/deferred/DeferredRenderingPipeline.kt
+++ pe-core/src/main/java/org/saar/core/renderer/deferred/DeferredRenderingPipeline.kt
@@ -1,21 +1,39 @@
 package org.saar.core.renderer.deferred
 
 import org.saar.core.camera.ICamera
-import org.saar.lwjgl.opengl.textures.ReadOnlyTexture
+import org.saar.core.screen.OffScreen
+import org.saar.core.screen.Screens
+import org.saar.lwjgl.opengl.fbos.Fbo
+import org.saar.lwjgl.opengl.textures.Texture
+import org.saar.lwjgl.opengl.utils.GlBuffer
+import org.saar.lwjgl.opengl.utils.GlUtils
 
 class DeferredRenderingPipeline(private vararg val renderPasses: RenderPass) {
 
-    fun render(camera: ICamera, colourTexture: ReadOnlyTexture, buffers: DeferredRenderingBuffers) {
+    private val prototype = RenderPassesScreenPrototype()
+
+    private val screen: OffScreen = Screens.fromPrototype(this.prototype, Fbo.create(1200, 700))
+
+    private val colourTexture: Texture
+        get() = this.prototype.colourTexture
+
+    fun process(camera: ICamera, buffers: DeferredRenderingBuffers): DeferredRenderingOutput {
+        this.screen.setAsDraw()
+        GlUtils.clear(GlBuffer.COLOUR)
+
         var currentBuffers = buffers
         for (renderPass in this.renderPasses) {
             renderPass.render(RenderPassContext(camera, currentBuffers))
 
-            currentBuffers = DeferredRenderingBuffers(colourTexture,
+            currentBuffers = DeferredRenderingBuffers(this.colourTexture,
                 currentBuffers.normal, currentBuffers.depth)
         }
+
+        return DeferredRenderingOutput(this.screen, this.colourTexture)
     }
 
     fun delete() {
+        this.screen.delete()
         this.renderPasses.forEach { it.delete() }
     }
 }
