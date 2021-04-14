df3a8c03e137c52f4ed88aee4c640c338ccf3a60
Saar Tako <saartako25@gmail.com>
2021-04-14 16:35:49 +0300

    added fxaa post processor

---
+++ pe-core/src/main/java/org/saar/core/postprocessing/processors/FxaaPostProcessor.kt
@@ -0,0 +1,45 @@
+package org.saar.core.postprocessing.processors
+
+import org.joml.Vector2i
+import org.saar.core.postprocessing.PostProcessingContext
+import org.saar.core.postprocessing.PostProcessor
+import org.saar.core.postprocessing.PostProcessorPrototype
+import org.saar.core.postprocessing.PostProcessorPrototypeWrapper
+import org.saar.core.renderer.uniforms.UniformProperty
+import org.saar.core.screen.MainScreen
+import org.saar.lwjgl.opengl.shaders.GlslVersion
+import org.saar.lwjgl.opengl.shaders.Shader
+import org.saar.lwjgl.opengl.shaders.ShaderCode
+import org.saar.lwjgl.opengl.shaders.uniforms.TextureUniformValue
+import org.saar.lwjgl.opengl.shaders.uniforms.Vec2iUniform
+
+class FxaaPostProcessor : PostProcessor,
+    PostProcessorPrototypeWrapper(FxaaPostProcessorPrototype())
+
+private class FxaaPostProcessorPrototype : PostProcessorPrototype {
+
+    @UniformProperty
+    private val textureUniform = TextureUniformValue("u_texture", 0)
+
+    @UniformProperty
+    private val resolutionUniform = object : Vec2iUniform() {
+        override fun getName() = "u_resolution"
+
+        override fun getUniformValue() = Vector2i(
+            MainScreen.getInstance().width,
+            MainScreen.getInstance().height)
+    }
+
+    override fun fragmentShader(): Shader = Shader.createFragment(GlslVersion.V400,
+        ShaderCode.define("FXAA_REDUCE_MIN", (1.0 / 128.0).toString()),
+        ShaderCode.define("FXAA_REDUCE_MUL", (1.0 / 8.0).toString()),
+        ShaderCode.define("FXAA_SPAN_MAX", 8.0.toString()),
+        ShaderCode.loadSource("/shaders/postprocessing/fxaa.pass.glsl"))
+
+    override fun onRender(context: PostProcessingContext) {
+        this.textureUniform.value = context.texture
+    }
+
+    override fun onDelete() {
+    }
+}
