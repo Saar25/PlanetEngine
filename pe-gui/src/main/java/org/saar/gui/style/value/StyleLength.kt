663bd5f247449697044768bc742f8b3e1a32d5ed
Saar Tako <saartako25@gmail.com>
2021-04-15 22:33:59 +0300

    added more StyleLength

--- pe-gui/src/main/java/org/saar/gui/style/value/StyleLength.kt
+++ pe-gui/src/main/java/org/saar/gui/style/value/StyleLength.kt
@@ -3,7 +3,7 @@ package org.saar.gui.style.value
 import org.saar.gui.style.property.Coordinate
 import org.saar.gui.style.property.Length
 
-interface StyleLength : StyleValue {
+fun interface StyleLength : StyleValue {
 
     fun compute(parentCoordinate: Coordinate, parentLength: Length): Int
 
--- pe-gui/src/main/java/org/saar/gui/style/value/StyleLengths.kt
+++ pe-gui/src/main/java/org/saar/gui/style/value/StyleLengths.kt
@@ -7,16 +7,42 @@ import org.saar.gui.style.property.Length
 object StyleLengths {
 
     @JvmStatic
-    fun pixels(pixels: Int) = object : StyleLength {
-        override fun compute(parentCoordinate: Coordinate, parentLength: Length): Int {
-            return parentCoordinate.get() + parentLength.get() + pixels
-        }
-    }
+    fun pixels(pixels: Int) = StyleLength { _: Coordinate, _: Length -> pixels }
 
     @JvmStatic
-    fun percent(percents: Float) = object : StyleLength {
-        override fun compute(parentCoordinate: Coordinate, parentLength: Length): Int {
-            return parentCoordinate.get() + (parentLength.get() * percents / 100).toInt()
+    fun percent(percents: Float) =
+        StyleLength { _: Coordinate, parentLength: Length ->
+            (parentLength.get() * percents / 100).toInt()
         }
-    }
+
+    @JvmStatic
+    fun inherit() = StyleLength { _: Coordinate, parentLength: Length -> parentLength.get() }
+
+    @JvmStatic
+    fun add(a: StyleLength, b: StyleLength) =
+        StyleLength { parentCoordinate: Coordinate, parentLength: Length ->
+            val aCompute = a.compute(parentCoordinate, parentLength)
+            val bCompute = b.compute(parentCoordinate, parentLength)
+            aCompute + bCompute
+        }
+
+    @JvmStatic
+    fun sub(a: StyleLength, b: StyleLength) =
+        StyleLength { parentCoordinate: Coordinate, parentLength: Length ->
+            val aCompute = a.compute(parentCoordinate, parentLength)
+            val bCompute = b.compute(parentCoordinate, parentLength)
+            aCompute - bCompute
+        }
+
+    @JvmStatic
+    fun add(a: StyleLength, b: Int) =
+        StyleLength { parentCoordinate: Coordinate, parentLength: Length ->
+            a.compute(parentCoordinate, parentLength) + b
+        }
+
+    @JvmStatic
+    fun sub(a: StyleLength, b: Int) =
+        StyleLength { parentCoordinate: Coordinate, parentLength: Length ->
+            a.compute(parentCoordinate, parentLength) - b
+        }
 }
