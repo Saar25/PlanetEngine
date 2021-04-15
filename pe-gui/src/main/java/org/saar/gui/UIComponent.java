4eff778ebf3c34fcb92861ba4c6834cf19c99ae1
Saar Tako <saartako25@gmail.com>
2021-04-15 21:32:06 +0300

    added style object and add(UIObject) method

--- pe-gui/src/main/java/org/saar/gui/UIComponent.java
+++ pe-gui/src/main/java/org/saar/gui/UIComponent.java
@@ -1,21 +1,29 @@
 package org.saar.gui;
 
-import java.util.Arrays;
+import org.saar.gui.style.Style;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * This class represent a ui component, with out any logic
  * use this class to group UIObjects and render them as one
  */
-public class UIComponent {
+public abstract class UIComponent {
 
-    private final List<UIObject> uiObjects;
+    private final Style style = new Style();
 
-    public UIComponent(UIObject... uiObjects) {
-        this.uiObjects = Arrays.asList(uiObjects);
+    private final List<UIObject> uiObjects = new ArrayList<>();
+
+    protected void add(UIObject uiObject) {
+        this.uiObjects.add(uiObject);
     }
 
     public List<UIObject> getUiObjects() {
         return this.uiObjects;
+    }
+
+    public Style getStyle() {
+        return this.style;
     }
 }
