80e76f1142c246823d12383543cee6133a4a3ddc
Saar Tako <saartako25@gmail.com>
2021-04-15 11:51:50 +0300

    created QuadMesh

---
+++ pe-core/src/main/java/org/saar/core/mesh/common/QuadMesh.kt
@@ -0,0 +1,17 @@
+package org.saar.core.mesh.common
+
+import org.saar.core.mesh.Mesh
+import org.saar.lwjgl.opengl.constants.RenderMode
+import org.saar.lwjgl.opengl.objects.vaos.Vao
+import org.saar.lwjgl.opengl.utils.GlRendering
+
+object QuadMesh : Mesh {
+
+    override fun draw() {
+        Vao.EMPTY.bind()
+        GlRendering.drawArrays(RenderMode.TRIANGLE_STRIP, 0, 4)
+    }
+
+    override fun delete() {
+    }
+}
