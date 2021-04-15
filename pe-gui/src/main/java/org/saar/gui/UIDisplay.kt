637e7d6dcad910bf577963afb3c6bb0376b7b8ec
Saar Tako <saartako25@gmail.com>
2021-04-15 23:17:56 +0300

    created UIDisplay

---
+++ pe-gui/src/main/java/org/saar/gui/UIDisplay.kt
@@ -0,0 +1,20 @@
+package org.saar.gui
+
+import org.saar.gui.render.UIRenderer
+import org.saar.gui.style.WindowStyle
+import org.saar.lwjgl.glfw.window.Window
+
+class UIDisplay(private val window: Window) {
+
+    private val style: WindowStyle = WindowStyle
+
+    private val uiComponents = mutableListOf<UIComponent>()
+
+    private val renderer = UIRenderer()
+
+    fun add(uiComponent: UIComponent) {
+        this.uiComponents.add(uiComponent)
+    }
+
+
+}
