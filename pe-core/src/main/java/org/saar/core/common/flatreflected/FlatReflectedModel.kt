68a57d878b528f466e43159c6c3f31de887dc09e
Saar Tako <saartako25@gmail.com>
2021-04-24 20:33:48 +0300

    refactored ReflectionExample

--- pe-core/src/main/java/org/saar/core/common/flatreflected/FlatReflectedModel.kt
+++ pe-core/src/main/java/org/saar/core/common/flatreflected/FlatReflectedModel.kt
@@ -1,7 +1,10 @@
 package org.saar.core.common.flatreflected
 
+import org.joml.Planef
 import org.joml.Vector3f
 import org.saar.core.mesh.Model
+import org.saar.lwjgl.opengl.textures.ReadOnlyTexture
+import org.saar.lwjgl.opengl.textures.Texture
 import org.saar.maths.transform.SimpleTransform
 
 class FlatReflectedModel(
@@ -9,5 +12,17 @@ class FlatReflectedModel(
     val normal: Vector3f,
     val transform: SimpleTransform) : Model {
 
+    var reflectionMap: ReadOnlyTexture = Texture.NULL
+
     constructor(mesh: FlatReflectedMesh, normal: Vector3f) : this(mesh, normal, SimpleTransform())
+
+    fun toPlane(): Planef {
+        val position = this.transform.position.value
+        return Planef(position, this.normal)
+    }
+
+    override fun delete() {
+        super.delete()
+        this.reflectionMap.delete()
+    }
 }
