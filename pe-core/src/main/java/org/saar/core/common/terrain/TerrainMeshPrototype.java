025c89f3e13203f2174dd0ea04b8739f752e9069
Saar Tako <saartako25@gmail.com>
2021-05-01 12:37:28 +0300

    removed redundant classes

--- pe-core/src/main/java/org/saar/core/common/terrain/TerrainMeshPrototype.java
+++
@@ -1,7 +0,0 @@
-package org.saar.core.common.terrain;
-
-import org.saar.core.mesh.build.MeshPrototype;
-
-public interface TerrainMeshPrototype extends MeshPrototype {
-
-}
--- pe-core/src/main/java/org/saar/core/common/terrain/TerrainVertex.java
+++
@@ -1,7 +0,0 @@
-package org.saar.core.common.terrain;
-
-import org.saar.core.mesh.Vertex;
-
-public interface TerrainVertex extends Vertex {
-
-}
