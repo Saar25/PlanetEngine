1702e7b32b7d4e99b63503baf3d9deebd99afc82
Saar Tako <saartako25@gmail.com>
2021-05-04 22:35:16 +0300

    created MoreDelegates.kt util file

---
+++ pe-util/src/main/java/org/saar/utils/MoreDelegates.kt
@@ -0,0 +1,19 @@
+package org.saar.utils
+
+import kotlin.properties.ObservableProperty
+import kotlin.reflect.KProperty
+
+object MoreDelegates {
+
+    fun clamp(initial: Float, min: Float = 0f, max: Float = 1f) = object : ObservableProperty<Float>(initial) {
+        override fun beforeChange(property: KProperty<*>, oldValue: Float, newValue: Float): Boolean {
+            return (min..max).contains(newValue)
+        }
+    }
+
+    fun clamp(initial: Int, min: Int = 0, max: Int = 1) = object : ObservableProperty<Int>(initial) {
+        override fun beforeChange(property: KProperty<*>, oldValue: Int, newValue: Int): Boolean {
+            return (min..max).contains(newValue)
+        }
+    }
+}
