fe8c7fe22d91878b86ff1e14b19650bed5becd1e
Saar Tako <saartako25@gmail.com>
2021-05-07 16:30:25 +0300

    added transforms

--- pe-math/src/main/java/org/saar/maths/transform/SimpleTransform.java
+++ pe-math/src/main/java/org/saar/maths/transform/SimpleTransform.java
@@ -6,6 +6,9 @@ import org.joml.Vector3f;
 import org.saar.maths.utils.Matrix4;
 import org.saar.maths.utils.Vector3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class SimpleTransform implements Transform {
 
     private final Matrix4f transformation = Matrix4.create();
@@ -13,6 +16,8 @@ public final class SimpleTransform implements Transform {
     private final Position position = Position.create();
     private final Rotation rotation = Rotation.create();
     private final Scale scale = Scale.create();
+
+    private final List<Transform> transforms = new ArrayList<>();
 
     public SimpleTransform() {
         getPosition().addListener(e -> updateTransformationMatrix());
@@ -26,6 +31,19 @@ public final class SimpleTransform implements Transform {
                 getRotation().getValue(),
                 getScale().getValue(),
                 this.transformation);
+
+        for (Transform transform : this.transforms) {
+            this.transformation.mul(transform
+                    .getTransformationMatrix());
+        }
+    }
+
+    public void addTransform(Transform transform) {
+        this.transforms.add(transform);
+        updateTransformationMatrix();
+        transform.getPosition().addListener(e -> updateTransformationMatrix());
+        transform.getRotation().addListener(e -> updateTransformationMatrix());
+        transform.getScale().addListener(e -> updateTransformationMatrix());
     }
 
     @Override
