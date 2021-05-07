0e36bb37ba27d9fb28cfb942dd82700372fcc96d
Saar Tako <saartako25@gmail.com>
2021-05-07 17:27:16 +0300

    transform behavior is now able to get transform as parameter

--- pe-core/src/main/java/org/saar/core/common/behaviors/TransformBehavior.kt
+++ pe-core/src/main/java/org/saar/core/common/behaviors/TransformBehavior.kt
@@ -4,8 +4,4 @@ import org.saar.core.behavior.Behavior
 import org.saar.maths.transform.SimpleTransform
 import org.saar.maths.transform.Transform
 
-class TransformBehavior : Behavior {
-
-    val transform: Transform = SimpleTransform()
-
-}
+class TransformBehavior(val transform: Transform = SimpleTransform()) : Behavior
