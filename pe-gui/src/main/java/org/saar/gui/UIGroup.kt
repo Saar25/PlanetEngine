c5d3e7d469a1c6f133e32d35e6ef911e7bf48794
Saar Tako <saartako25@gmail.com>
2021-05-21 17:58:31 +0300

    removed parent parameter in UIGroup

--- pe-gui/src/main/java/org/saar/gui/UIGroup.kt
+++ pe-gui/src/main/java/org/saar/gui/UIGroup.kt
@@ -2,13 +2,13 @@ package org.saar.gui
 
 import org.saar.gui.style.Style
 
-class UIGroup(parent: UIElement) : UIContainer, UIChildNode {
+class UIGroup : UIContainer, UIChildNode {
 
     override val children = mutableListOf<UINode>()
 
     override val style = Style(this)
 
-    override var parent: UIElement = parent
+    override var parent: UIElement = UINullElement
 
     fun add(uiNode: UIChildNode) {
         this.children.add(uiNode)
