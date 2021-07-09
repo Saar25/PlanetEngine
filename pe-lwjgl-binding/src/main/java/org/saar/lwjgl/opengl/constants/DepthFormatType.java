55c5621cc11d54a4213e2f40014aeabc9332b2e4
Saar Tako <saartako25@gmail.com>
2021-07-09 14:14:30 +0300

    added stencil depth formatsi

--- pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/constants/DepthFormatType.java
+++ pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/constants/DepthFormatType.java
@@ -5,6 +5,8 @@ public enum DepthFormatType {
     COMPONENT16(InternalFormat.DEPTH16),
     COMPONENT24(InternalFormat.DEPTH24),
     COMPONENT32(InternalFormat.DEPTH32),
+    COMPONENT24_STENCIL8(InternalFormat.DEPTH24_STENCIL8),
+    COMPONENT32F_STENCIL8(InternalFormat.DEPTH32F_STENCIL8),
     ;
 
     private final InternalFormat value;
