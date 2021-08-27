1dc97ebcbab9941cd769a80a6ba299c1ad4f84c0
Saar Tako <saartako25@gmail.com>
2021-08-27 16:30:57 +0300

    process only rendered pixels in deferred render passes

--- pe-core/src/main/java/org/saar/core/renderer/deferred/DeferredRenderPassesPipeline.kt
+++ pe-core/src/main/java/org/saar/core/renderer/deferred/DeferredRenderPassesPipeline.kt
@@ -5,7 +5,11 @@ import org.saar.core.renderer.renderpass.RenderPassContext
 import org.saar.core.renderer.renderpass.RenderPassScreenPrototype
 import org.saar.core.screen.OffScreen
 import org.saar.core.screen.Screens
+import org.saar.lwjgl.opengl.constants.Comparator
+import org.saar.lwjgl.opengl.depth.DepthTest
 import org.saar.lwjgl.opengl.fbos.Fbo
+import org.saar.lwjgl.opengl.fbos.FboBlitFilter
+import org.saar.lwjgl.opengl.stencil.*
 import org.saar.lwjgl.opengl.textures.Texture
 import org.saar.lwjgl.opengl.utils.GlBuffer
 import org.saar.lwjgl.opengl.utils.GlUtils
@@ -19,23 +23,33 @@ class DeferredRenderPassesPipeline(private vararg val renderPasses: DeferredRend
     private val colourTexture: Texture
         get() = this.prototype.colourTexture
 
-    fun process(camera: ICamera, buffers: DeferredRenderingBuffers): DeferredRenderingOutput {
-        this.screen.resizeToMainScreen()
+    private val stencilState = StencilState(StencilOperation.ALWAYS_KEEP,
+        StencilFunction(Comparator.NOT_EQUAL, 0, 0xFF), StencilMask.UNCHANGED)
+
+    fun process(camera: ICamera, inputBuffers: DeferredRenderingBuffers, screen: OffScreen): DeferredRenderingOutput {
+        this.screen.assureSize(screen.width, screen.height)
+        screen.copyTo(this.screen, FboBlitFilter.NEAREST, GlBuffer.DEPTH, GlBuffer.STENCIL)
 
         this.screen.setAsDraw()
-        GlUtils.clear(GlBuffer.COLOUR)
+        GlUtils.clear(GlBuffer.COLOUR, GlBuffer.DEPTH)
 
         val context = RenderPassContext(camera)
 
-        var currentBuffers = buffers
-        for (renderPass in this.renderPasses) {
-            renderPass.render(context, currentBuffers)
+        val outputBuffers = runPipeline(inputBuffers, context)
 
-            currentBuffers = DeferredRenderingBuffers(this.colourTexture,
-                currentBuffers.normal, currentBuffers.specular, currentBuffers.depth)
+        return DeferredRenderingOutput(this.screen, outputBuffers)
+    }
+
+    private fun runPipeline(input: DeferredRenderingBuffers, context: RenderPassContext): DeferredRenderingBuffers {
+        return this.renderPasses.fold(input) { buffers, renderPass ->
+            StencilTest.apply(this.stencilState)
+            DepthTest.disable()
+
+            renderPass.render(context, buffers)
+
+            DeferredRenderingBuffers(this.colourTexture,
+                buffers.normal, buffers.specular, buffers.depth)
         }
-
-        return DeferredRenderingOutput(this.screen, currentBuffers)
     }
 
     fun delete() {
--- pe-core/src/main/java/org/saar/core/renderer/deferred/DeferredRenderingPath.java
+++ pe-core/src/main/java/org/saar/core/renderer/deferred/DeferredRenderingPath.java
@@ -5,7 +5,10 @@ import org.saar.core.renderer.RenderContextBase;
 import org.saar.core.renderer.RenderingPath;
 import org.saar.core.screen.OffScreen;
 import org.saar.core.screen.Screens;
+import org.saar.lwjgl.opengl.constants.Comparator;
 import org.saar.lwjgl.opengl.fbos.Fbo;
+import org.saar.lwjgl.opengl.stencil.*;
+import org.saar.lwjgl.opengl.utils.GlBuffer;
 import org.saar.lwjgl.opengl.utils.GlUtils;
 
 public class DeferredRenderingPath implements RenderingPath {
@@ -13,6 +16,9 @@ public class DeferredRenderingPath implements RenderingPath {
     private final DeferredScreenPrototype prototype = new DeferredScreenPrototype();
 
     private final OffScreen screen = Screens.fromPrototype(this.prototype, Fbo.create(0, 0));
+
+    private final StencilState stencilState = new StencilState(StencilOperation.REPLACE_ON_PASS,
+            new StencilFunction(Comparator.ALWAYS, 1, 0xFF), StencilMask.UNCHANGED);
 
     private final ICamera camera;
     private final DeferredRenderNode renderNode;
@@ -30,11 +36,13 @@ public class DeferredRenderingPath implements RenderingPath {
         this.screen.setAsDraw();
         this.screen.resizeToMainScreen();
 
-        GlUtils.clearColourAndDepthBuffer();
+        GlUtils.clear(GlBuffer.COLOUR, GlBuffer.DEPTH, GlBuffer.STENCIL);
+
+        StencilTest.apply(this.stencilState);
 
         this.renderNode.renderDeferred(new RenderContextBase(this.camera));
 
-        return this.pipeline.process(this.camera, this.prototype.asBuffers());
+        return this.pipeline.process(this.camera, this.prototype.asBuffers(), screen);
     }
 
     @Override
