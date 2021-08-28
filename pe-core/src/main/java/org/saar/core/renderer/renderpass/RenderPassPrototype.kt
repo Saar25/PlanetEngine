c6ae346de15ddfb03f4fb7789480c105a5ae9202
Saar Tako <saartako25@gmail.com>
2021-08-28 19:39:05 +0300

    allowing custom vertex shader

--- pe-core/src/main/java/org/saar/core/renderer/renderpass/RenderPassPrototype.kt
+++ pe-core/src/main/java/org/saar/core/renderer/renderpass/RenderPassPrototype.kt
@@ -1,8 +1,15 @@
 package org.saar.core.renderer.renderpass
 
+import org.saar.lwjgl.opengl.shaders.GlslVersion
 import org.saar.lwjgl.opengl.shaders.Shader
+import org.saar.lwjgl.opengl.shaders.ShaderCode
+
+private val vertexShaderCode = ShaderCode.loadSource(
+    "/shaders/common/quad/quad.vertex.glsl")
 
 interface RenderPassPrototype<T : RenderPassBuffers> {
+
+    fun vertexShader(): Shader = Shader.createVertex(GlslVersion.V400, vertexShaderCode)
 
     fun fragmentShader(): Shader
 
