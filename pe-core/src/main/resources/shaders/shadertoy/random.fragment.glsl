ae6807e1a6475794cdf19879c1e0f841f40d6907
Saar Tako <saartako25@gmail.com>
2021-09-08 12:19:26 +0300

    updated random shader sources

--- pe-core/src/main/resources/shaders/shadertoy/random.fragment.glsl
+++ pe-core/src/main/resources/shaders/shadertoy/random.fragment.glsl
@@ -7,7 +7,7 @@ in vec2 v_position;
 layout (location = 0) out vec4 f_colour;
 
 void main(void) {
-    float random = random1f(v_position.xyxy);
+    float random = random(v_position.xyxy);
 
     f_colour = vec4(vec3(random), 1);
 }
