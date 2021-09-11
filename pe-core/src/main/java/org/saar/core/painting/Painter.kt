9068125ab0cbf6e97226ae257e85573429458589
Saar Tako <saartako25@gmail.com>
2021-09-11 15:49:37 +0300

    added extension method Painter::renderToTexture

--- pe-core/src/main/java/org/saar/core/painting/Painter.kt
+++ pe-core/src/main/java/org/saar/core/painting/Painter.kt
@@ -8,6 +8,13 @@ import org.saar.core.renderer.forward.ForwardRenderPass
 import org.saar.core.renderer.forward.ForwardRenderingBuffers
 import org.saar.core.renderer.renderpass.RenderPass
 import org.saar.core.renderer.renderpass.RenderPassContext
+import org.saar.lwjgl.opengl.constants.ColourFormatType
+import org.saar.lwjgl.opengl.constants.DataType
+import org.saar.lwjgl.opengl.constants.FormatType
+import org.saar.lwjgl.opengl.fbos.Fbo
+import org.saar.lwjgl.opengl.fbos.attachment.ColourAttachment
+import org.saar.lwjgl.opengl.textures.Texture
+import org.saar.lwjgl.opengl.textures.TextureTarget
 
 interface Painter : RenderPass, PostProcessor, ForwardRenderPass, DeferredRenderPass {
 
@@ -18,4 +25,23 @@ interface Painter : RenderPass, PostProcessor, ForwardRenderPass, DeferredRender
     override fun render(context: RenderPassContext, buffers: ForwardRenderingBuffers) = render()
 
     override fun render(context: RenderPassContext, buffers: DeferredRenderingBuffers) = render()
+}
+
+fun Painter.renderToTexture(width: Int, height: Int,
+                            colourFormatType: ColourFormatType = ColourFormatType.RGB8,
+                            formatType: FormatType = FormatType.RGB,
+                            dataType: DataType = DataType.U_BYTE): Texture {
+    return Texture.create(TextureTarget.TEXTURE_2D).also { texture ->
+        val fbo = Fbo.create(width, height).apply {
+            val attachment = ColourAttachment.withTexture(0,
+                texture, colourFormatType, formatType, dataType)
+            addAttachment(attachment)
+            setDrawAttachments(attachment)
+            setReadAttachment(attachment)
+        }
+
+        fbo.bindAsDraw()
+        render()
+        fbo.delete()
+    }
 }
