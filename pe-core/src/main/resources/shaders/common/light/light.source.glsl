91289ec56c0818c959360bc1ace39bf3fc3c62fd
Saar Tako <saartako25@gmail.com>
2021-09-15 16:00:35 +0300

    fixed specular light

--- pe-core/src/main/resources/shaders/common/light/light.source.glsl
+++ pe-core/src/main/resources/shaders/common/light/light.source.glsl
@@ -33,13 +33,13 @@ float smoothSpecularFactor(const float specular, const float power, const float 
 // Directional light
 
 float lightFactor(const DirectionalLight light, const vec3 normal, const vec3 reflectedViewDirection, const float power, const float scalar) {
-    float diffuse = ambientFactor() + diffuseFactor(normal, light.direction);
+    float diffuse = diffuseFactor(normal, light.direction);
     if (diffuse > 0) {
         float specularFactor = specularFactor(light.direction, reflectedViewDirection);
         float smoothSpecularFactor = smoothSpecularFactor(specularFactor, power, scalar);
-        return diffuse + smoothSpecularFactor;
+        return ambientFactor() + diffuse + smoothSpecularFactor;
     } else {
-        return diffuse;
+        return ambientFactor() + diffuse;
     }
 }
 
@@ -49,7 +49,7 @@ vec3 lightColour(const DirectionalLight light, const vec3 normal, const vec3 ref
 
 vec3 totalLightsColour(const DirectionalLight[MAX_DIRECTIONAL_LIGHTS] lights, const int count,
                        const vec3 normal, const vec3 viewDirection, const float power, const float scalar) {
-    vec3 reflectedViewDirection = reflect(-viewDirection, normal);
+    vec3 reflectedViewDirection = reflect(viewDirection, normal);
     vec3 colour = vec3(0, 0, 0);
     for (int i = 0; i < count; i++) {
         colour += lightColour(lights[i], normal, reflectedViewDirection, power, scalar);
@@ -66,13 +66,13 @@ float lightFactor(const PointLight light, const vec3 normal, const vec3 reflecte
     float attenuation = a.x + a.y * d + a.z * d * d;
     vec3 direction = normalize(viewPosition - light.position);
     
-    float diffuse = ambientFactor() + diffuseFactor(normal, direction);
+    float diffuse = diffuseFactor(normal, direction);
     if (diffuse > 0) {
         float specularFactor = specularFactor(direction, reflectedViewDirection);
         float smoothSpecularFactor = smoothSpecularFactor(specularFactor, power, scalar);
-        return (diffuse + smoothSpecularFactor) / attenuation;
+        return (ambientFactor() + diffuse + smoothSpecularFactor) / attenuation;
     } else {
-        return diffuse / attenuation;
+        return (ambientFactor() + diffuse) / attenuation;
     }
 }
 
@@ -83,7 +83,7 @@ vec3 lightColour(const PointLight light, const vec3 normal, const vec3 reflected
 
 vec3 totalLightsColour(const PointLight[MAX_POINT_LIGHTS] lights, const int count, const vec3 normal,
                        const vec3 viewDirection, const vec3 viewPosition, const float power, const float scalar) {
-    vec3 reflectedViewDirection = reflect(-viewDirection, normal);
+    vec3 reflectedViewDirection = reflect(viewDirection, normal);
     vec3 colour = vec3(0, 0, 0);
     for (int i = 0; i < count; i++) {
         colour += lightColour(lights[i], normal, viewDirection, viewPosition, power, scalar);
