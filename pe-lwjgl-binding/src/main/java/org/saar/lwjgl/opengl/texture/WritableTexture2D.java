9cd1d00b13d063721f8f5cc21be431a9476e0345
Saar Tako <saartako25@gmail.com>
2021-09-18 17:00:06 +0300

    created ReadOnlyTexture2D

--- pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/texture/WritableTexture2D.java
+++ pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/texture/WritableTexture2D.java
@@ -5,7 +5,7 @@ import org.saar.lwjgl.opengl.constants.FormatType;
 
 import java.nio.ByteBuffer;
 
-public interface WritableTexture2D extends MutableTexture {
+public interface WritableTexture2D extends MutableTexture, ReadOnlyTexture2D {
 
     void load(int level, int xOffset, int yOffset, int width,
               int height, FormatType format, DataType type, ByteBuffer data);
@@ -13,9 +13,5 @@ public interface WritableTexture2D extends MutableTexture {
     default void load(int level, FormatType format, DataType type, ByteBuffer data) {
         load(level, 0, 0, getWidth(), getHeight(), format, type, data);
     }
-
-    int getWidth();
-
-    int getHeight();
 
 }
