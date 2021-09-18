dde3799d2952695e3906a486cbe948f8c5010fcb
Saar Tako <saartako25@gmail.com>
2021-09-18 14:09:36 +0300

    using modifiers from glfw package

--- pe-gui/src/main/kotlin/org/saar/gui/event/Modifiers.kt
+++
@@ -1,7 +0,0 @@
-package org.saar.gui.event
-
-data class Modifiers(
-    val alt: Boolean = false,
-    val shift: Boolean = false,
-    val ctrl: Boolean = false,
-)
--- pe-gui/src/main/kotlin/org/saar/gui/event/MouseEvent.kt
+++ pe-gui/src/main/kotlin/org/saar/gui/event/MouseEvent.kt
@@ -1,5 +1,6 @@
 package org.saar.gui.event
 
+import org.saar.lwjgl.glfw.input.Modifiers
 import org.saar.lwjgl.glfw.input.mouse.ClickEvent
 import org.saar.lwjgl.glfw.input.mouse.MouseButton
 import org.saar.lwjgl.glfw.input.mouse.MoveEvent
@@ -7,7 +8,7 @@ import org.saar.lwjgl.glfw.window.Window
 
 class MouseEvent(
     val button: MouseButton = MouseButton.NONE,
-    val modifiers: Modifiers = Modifiers(),
+    val modifiers: Modifiers = Modifiers(0),
     val x: Int,
     val y: Int,
     val xBefore: Int,
@@ -20,17 +21,18 @@ class MouseEvent(
 }
 
 fun ClickEvent.asMouseEvent() = MouseEvent(
+    button = this.button,
+    modifiers = this.modifiers,
     x = this.mouse.xPos,
     y = this.mouse.xPos,
     xBefore = this.mouse.xPos,
     yBefore = this.mouse.yPos,
-    button = this.button
 )
 
 fun MoveEvent.asMouseEvent() = MouseEvent(
+    button = if (this.mouse.isButtonDown(MouseButton.PRIMARY)) MouseButton.PRIMARY else MouseButton.NONE,
     x = this.x.after,
     y = this.y.after,
     xBefore = this.x.before,
-    yBefore = this.y.before,
-    button = if (this.mouse.isButtonDown(MouseButton.PRIMARY)) MouseButton.PRIMARY else MouseButton.NONE
+    yBefore = this.y.before
 )
