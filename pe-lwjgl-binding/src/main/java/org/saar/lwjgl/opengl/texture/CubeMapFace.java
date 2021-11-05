d5f8f8e7a4fb8a101b65e592bc67962072dd5ee0
Saar Tako <saartako25@gmail.com>
2021-11-05 12:53:53 +0200

    using java 11

--- pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/texture/CubeMapFace.java
+++ pe-lwjgl-binding/src/main/java/org/saar/lwjgl/opengl/texture/CubeMapFace.java
@@ -19,15 +19,21 @@ public enum CubeMapFace {
     }
 
     public static CubeMapFace ofCubeFace(int face) {
-        return switch (face) {
-            case 0 -> CubeMapFace.POSITIVE_X;
-            case 1 -> CubeMapFace.NEGATIVE_X;
-            case 2 -> CubeMapFace.POSITIVE_Y;
-            case 3 -> CubeMapFace.NEGATIVE_Y;
-            case 4 -> CubeMapFace.POSITIVE_Z;
-            case 5 -> CubeMapFace.NEGATIVE_Z;
-            default -> throw new IllegalArgumentException("Cubes do not have face " + face);
-        };
+        switch (face) {
+            case 0:
+                return CubeMapFace.POSITIVE_X;
+            case 1:
+                return CubeMapFace.NEGATIVE_X;
+            case 2:
+                return CubeMapFace.POSITIVE_Y;
+            case 3:
+                return CubeMapFace.NEGATIVE_Y;
+            case 4:
+                return CubeMapFace.POSITIVE_Z;
+            case 5:
+                return CubeMapFace.NEGATIVE_Z;
+        }
+        throw new IllegalArgumentException("Cubes do not have face " + face);
     }
 
     public int get() {
