0f072401ef996ba736f28790ed6a195a19983502
Saar Tako <saartako25@gmail.com>
2021-12-11 15:09:29 +0200

    implemented ParticlesSphereExample

---
+++ pe-core/src/main/java/org/saar/core/common/components/FocusRotationComponent.kt
@@ -0,0 +1,19 @@
+package org.saar.core.common.components
+
+import org.saar.core.node.ComposableNode
+import org.saar.core.node.NodeComponent
+import org.saar.maths.transform.ReadonlyPosition
+import org.saar.maths.transform.lookAt
+
+class FocusRotationComponent(private val focus: ReadonlyPosition) : NodeComponent {
+
+    private lateinit var transformComponent: TransformComponent
+
+    override fun start(node: ComposableNode) {
+        this.transformComponent = node.components.get()
+    }
+
+    override fun update(node: ComposableNode) {
+        this.transformComponent.transform.lookAt(focus)
+    }
+}
--- pe-math/src/main/java/org/saar/maths/transform/Transform.kt
+++ pe-math/src/main/java/org/saar/maths/transform/Transform.kt
@@ -1,10 +1,17 @@
 package org.saar.maths.transform
 
 import org.joml.Matrix4fc
+import org.saar.maths.utils.Vector3
 
 interface Transform : ReadonlyTransform {
     override val transformationMatrix: Matrix4fc
     override val position: Position
     override val rotation: Rotation
     override val scale: Scale
+}
+
+
+fun Transform.lookAt(position: ReadonlyPosition) {
+    val direction = Vector3.sub(position.value, this.position.value)
+    this.rotation.lookAlong(direction)
 }
