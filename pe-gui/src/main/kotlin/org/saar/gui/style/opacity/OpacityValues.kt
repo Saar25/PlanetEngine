2fbbbd09480754cd0315ac3e07cd398b3336d5b1
Saar Tako <saartako25@gmail.com>
2022-11-05 12:58:14 +0200

    added transition value

--- pe-gui/src/main/kotlin/org/saar/gui/style/opacity/OpacityValues.kt
+++ pe-gui/src/main/kotlin/org/saar/gui/style/opacity/OpacityValues.kt
@@ -1,5 +1,7 @@
 package org.saar.gui.style.opacity
 
+import kotlin.time.Duration
+
 object OpacityValues {
 
     @JvmField
@@ -8,4 +10,10 @@ object OpacityValues {
     @JvmStatic
     fun of(opacity: Float): OpacityValue = OpacityValue { opacity }
 
+    @JvmStatic
+    fun transition(from: Float, to: Float, time: Duration, start: Long = System.currentTimeMillis()) = OpacityValue {
+        val delta = System.currentTimeMillis() - start
+        val percent = delta / time.inWholeMilliseconds.toFloat()
+        percent.coerceIn(0f, 1f) * (to - from) + from
+    }
 }
