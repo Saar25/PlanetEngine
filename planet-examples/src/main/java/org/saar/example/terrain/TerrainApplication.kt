299ddef1e07c4ffe16b9ba86b8c57f2eb5e22114
Saar Tako <saartako25@gmail.com>
2023-07-22 19:18:38 +0300

    using application api instead of example

--- planet-examples/src/main/java/org/saar/example/terrain/TerrainApplication.kt
+++ planet-examples/src/main/java/org/saar/example/terrain/TerrainApplication.kt
@@ -1,187 +1,186 @@
-package org.saar.example.terrain;
+package org.saar.example.terrain
 
-import org.joml.SimplexNoise;
-import org.joml.Vector2i;
-import org.saar.core.camera.Camera;
-import org.saar.core.camera.ICamera;
-import org.saar.core.camera.Projection;
-import org.saar.core.camera.projection.ScreenPerspectiveProjection;
-import org.saar.core.common.components.KeyboardMovementComponent;
-import org.saar.core.common.components.KeyboardMovementScrollVelocityComponent;
-import org.saar.core.common.components.MouseDragRotationComponent;
-import org.saar.core.common.r3d.Instance3D;
-import org.saar.core.common.r3d.Model3D;
-import org.saar.core.common.r3d.Node3D;
-import org.saar.core.common.r3d.R3D;
-import org.saar.core.common.terrain.World;
-import org.saar.core.common.terrain.colour.ColourGenerator;
-import org.saar.core.common.terrain.colour.NormalColour;
-import org.saar.core.common.terrain.colour.NormalColourGenerator;
-import org.saar.core.common.terrain.height.HeightGenerator;
-import org.saar.core.common.terrain.height.NoiseHeightGenerator;
-import org.saar.core.common.terrain.lowpoly.LowPolyTerrainFactory;
-import org.saar.core.common.terrain.lowpoly.LowPolyWorld;
-import org.saar.core.common.terrain.mesh.DiamondMeshGenerator;
-import org.saar.core.fog.Fog;
-import org.saar.core.fog.FogDistance;
-import org.saar.core.light.DirectionalLight;
-import org.saar.core.mesh.Mesh;
-import org.saar.core.node.NodeComponentGroup;
-import org.saar.core.postprocessing.processors.FxaaPostProcessor;
-import org.saar.core.postprocessing.processors.SkyboxPostProcessor;
-import org.saar.core.renderer.deferred.DeferredRenderNode;
-import org.saar.core.renderer.deferred.DeferredRenderNodeGroup;
-import org.saar.core.renderer.deferred.DeferredRenderingPath;
-import org.saar.core.renderer.deferred.DeferredRenderingPipeline;
-import org.saar.core.renderer.deferred.passes.DeferredGeometryPass;
-import org.saar.core.renderer.deferred.passes.LightRenderPass;
-import org.saar.core.renderer.deferred.passes.SsaoRenderPass;
-import org.saar.core.renderer.forward.passes.FogRenderPass;
-import org.saar.core.util.Fps;
-import org.saar.example.ExamplesUtils;
-import org.saar.lwjgl.glfw.input.keyboard.Keyboard;
-import org.saar.lwjgl.glfw.input.mouse.Mouse;
-import org.saar.lwjgl.glfw.window.Window;
-import org.saar.lwjgl.opengl.clear.ClearColour;
-import org.saar.lwjgl.opengl.texture.CubeMapTexture;
-import org.saar.lwjgl.opengl.texture.CubeMapTextureBuilder;
-import org.saar.maths.noise.LayeredNoise2f;
-import org.saar.maths.noise.MultipliedNoise2f;
-import org.saar.maths.noise.SpreadNoise2f;
-import org.saar.maths.transform.Position;
-import org.saar.maths.utils.Vector2;
-import org.saar.maths.utils.Vector3;
+import org.joml.SimplexNoise
+import org.joml.Vector2i
+import org.saar.core.camera.Camera
+import org.saar.core.camera.ICamera
+import org.saar.core.camera.Projection
+import org.saar.core.camera.projection.ScreenPerspectiveProjection
+import org.saar.core.common.components.KeyboardMovementComponent
+import org.saar.core.common.components.KeyboardMovementScrollVelocityComponent
+import org.saar.core.common.components.MouseDragRotationComponent
+import org.saar.core.common.r3d.Model3D
+import org.saar.core.common.r3d.Node3D
+import org.saar.core.common.r3d.R3D.instance
+import org.saar.core.common.r3d.R3D.mesh
+import org.saar.core.common.terrain.World
+import org.saar.core.common.terrain.colour.ColourGenerator
+import org.saar.core.common.terrain.colour.NormalColour
+import org.saar.core.common.terrain.colour.NormalColourGenerator
+import org.saar.core.common.terrain.height.HeightGenerator
+import org.saar.core.common.terrain.height.NoiseHeightGenerator
+import org.saar.core.common.terrain.lowpoly.LowPolyTerrainFactory
+import org.saar.core.common.terrain.lowpoly.LowPolyWorld
+import org.saar.core.common.terrain.mesh.DiamondMeshGenerator
+import org.saar.core.engine.Application
+import org.saar.core.engine.PlanetEngine
+import org.saar.core.fog.Fog
+import org.saar.core.fog.FogDistance
+import org.saar.core.light.DirectionalLight
+import org.saar.core.node.NodeComponentGroup
+import org.saar.core.postprocessing.processors.FxaaPostProcessor
+import org.saar.core.postprocessing.processors.SkyboxPostProcessor
+import org.saar.core.renderer.deferred.DeferredRenderNode
+import org.saar.core.renderer.deferred.DeferredRenderNodeGroup
+import org.saar.core.renderer.deferred.DeferredRenderingPath
+import org.saar.core.renderer.deferred.DeferredRenderingPipeline
+import org.saar.core.renderer.deferred.passes.DeferredGeometryPass
+import org.saar.core.renderer.deferred.passes.LightRenderPass
+import org.saar.core.renderer.deferred.passes.SsaoRenderPass
+import org.saar.core.renderer.forward.passes.FogRenderPass
+import org.saar.core.util.Fps
+import org.saar.example.ExamplesUtils
+import org.saar.lwjgl.glfw.window.Window
+import org.saar.lwjgl.opengl.clear.ClearColour
+import org.saar.lwjgl.opengl.texture.CubeMapTexture
+import org.saar.lwjgl.opengl.texture.CubeMapTextureBuilder
+import org.saar.maths.noise.LayeredNoise2f
+import org.saar.maths.noise.MultipliedNoise2f
+import org.saar.maths.noise.SpreadNoise2f
+import org.saar.maths.transform.Position
+import org.saar.maths.utils.Vector2
+import org.saar.maths.utils.Vector3
 
-import java.io.IOException;
+private const val WIDTH = 1200
+private const val HEIGHT = 700
+private const val MAX_DISTANCE_CLIP = 1_000f
 
-public class TerrainExample {
+fun main() {
+    val application = TerrainApplication()
+    val engine = PlanetEngine()
+    engine.run(application)
+}
 
-    private static final int WIDTH = 1200;
-    private static final int HEIGHT = 700;
+private class TerrainApplication : Application {
 
-    public static void main(String[] args) throws Exception {
-        final Window window = Window.create("Lwjgl", WIDTH, HEIGHT, true);
+    private lateinit var fps: Fps
+    private lateinit var camera: Camera
+    private lateinit var renderingPath: DeferredRenderingPath
+    private lateinit var cameraMovementComponent: KeyboardMovementComponent
 
-        ClearColour.set(.0f, .7f, .8f);
+    override fun initialize(window: Window) {
+        window.width = WIDTH
+        window.height = HEIGHT
 
-        final Keyboard keyboard = window.getKeyboard();
-        final Mouse mouse = window.getMouse();
+        ClearColour.set(.0f, .7f, .8f)
 
-        final Projection projection = new ScreenPerspectiveProjection(70f, 1, 1000);
+        val keyboard = window.keyboard
+        val mouse = window.mouse
 
-        final KeyboardMovementComponent cameraMovementComponent =
-                new KeyboardMovementComponent(keyboard, 50f, 50f, 50f);
-        final NodeComponentGroup components = new NodeComponentGroup(cameraMovementComponent,
-                new KeyboardMovementScrollVelocityComponent(mouse),
-                new MouseDragRotationComponent(mouse, -.3f));
+        val projection: Projection = ScreenPerspectiveProjection(70f, 1f, MAX_DISTANCE_CLIP)
+        this.cameraMovementComponent = KeyboardMovementComponent(keyboard, 50f, 50f, 50f)
+        val components = NodeComponentGroup(cameraMovementComponent,
+            KeyboardMovementScrollVelocityComponent(mouse),
+            MouseDragRotationComponent(mouse, -.3f))
 
-        final Camera camera = new Camera(projection, components);
-        camera.getTransform().getPosition().set(0, 0, 200);
-        camera.getTransform().lookAt(Position.of(0, 0, 0));
+        this.camera = Camera(projection, components)
 
-        final LowPolyWorld world = buildWorld();
+        camera.transform.position[0f, 0f] = 200f
+        camera.transform.lookAt(Position.of(0f, 0f, 0f))
 
-        for (int x = -5; x <= 5; x++) {
-            for (int z = -5; z <= 5; z++) {
-                world.createTerrain(new Vector2i(x, z));
+        val world = buildWorld()
+        for (x in -5..5) {
+            for (z in -5..5) {
+                world.createTerrain(Vector2i(x, z))
             }
         }
 
-        final Model3D cubeModel = buildCubeModel(world);
-        final Node3D cube = new Node3D(cubeModel);
-
-        final Model3D cubeModel2 = buildCubeModel(world);
-        cubeModel2.getTransform().getPosition().addX(-5);
-        cubeModel2.getTransform().getPosition().addY(5);
-        final Node3D cube2 = new Node3D(cubeModel2);
-
-        final DirectionalLight light = buildDirectionalLight();
-
-        final CubeMapTexture cubeMap = createCubeMap();
-        final DeferredRenderNodeGroup renderNode = new DeferredRenderNodeGroup(cube, cube2, world);
-        final DeferredRenderingPath renderingPath = buildRenderingPath(camera, renderNode, light, cubeMap);
-
-        final Fps fps = new Fps();
-        while (window.isOpen() && !keyboard.isKeyPressed('T')) {
-            camera.update();
-
-            renderingPath.render().toMainScreen();
-
-            window.swapBuffers();
-            window.pollEvents();
-
-            final double delta = fps.delta() * 1000;
-
-            System.out.print("\r --> " +
-                    "Speed: " + String.format("%.2f", cameraMovementComponent.getVelocity().x()) +
-                    ", Fps: " + String.format("%.2f", fps.fps()) +
-                    ", Delta: " + delta);
-            fps.update();
-        }
-
-        camera.delete();
-        renderingPath.delete();
-        window.destroy();
+        val cubeModel = buildCubeModel(world)
+        val cube = Node3D(cubeModel)
+        val cubeModel2 = buildCubeModel(world)
+        cubeModel2.transform.position.addX(-5f)
+        cubeModel2.transform.position.addY(5f)
+        val cube2 = Node3D(cubeModel2)
+        val light = buildDirectionalLight()
+        val cubeMap = createCubeMap()
+        val renderNode = DeferredRenderNodeGroup(cube, cube2, world)
+        this.renderingPath = buildRenderingPath(camera, renderNode, light, cubeMap)
+        this.fps = Fps()
     }
 
-    private static LowPolyWorld buildWorld() {
-        final HeightGenerator heightGenerator = new NoiseHeightGenerator(
-                new MultipliedNoise2f(200, new SpreadNoise2f(50,
-                        new LayeredNoise2f(SimplexNoise::noise, 5)))
-        );
+    override fun update(window: Window) {
+        camera.update()
 
-        final ColourGenerator colourGenerator = new NormalColourGenerator(Vector3.upward(),
-                new NormalColour(0.5f, Vector3.of(.41f, .41f, .41f)),
-                new NormalColour(1.0f, Vector3.of(.07f, .52f, .06f)));
-
-        final LowPolyTerrainFactory terrainFactory = new LowPolyTerrainFactory(
-                new DiamondMeshGenerator(64), heightGenerator,
-                colourGenerator, Vector2.of(256, 256)
-        );
-
-        return new LowPolyWorld(terrainFactory);
+        val delta = fps.delta() * 1000
+        print("\r --> " +
+                "Speed: " + String.format("%.2f", cameraMovementComponent.velocity.x()) +
+                ", Fps: " + String.format("%.2f", fps.fps()) +
+                ", Delta: " + delta)
+        fps.update()
     }
 
-    private static Model3D buildCubeModel(World world) {
-        final Instance3D cubeInstance = R3D.instance();
-        cubeInstance.getTransform().getScale().set(10, 10, 10);
-        cubeInstance.getTransform().getPosition().set(101, world.getHeight(101, 0, 50), 50);
-        final Mesh cubeMesh = R3D.mesh(new Instance3D[]{cubeInstance},
-                ExamplesUtils.cubeVertices, ExamplesUtils.cubeIndices);
-        return new Model3D(cubeMesh);
+    override fun render(window: Window) {
+        renderingPath.render().toMainScreen()
     }
 
-    private static DirectionalLight buildDirectionalLight() {
-        final DirectionalLight light = new DirectionalLight();
-        light.getDirection().set(-1, -1, -1);
-        light.getColour().set(1, 1, 1);
-        return light;
+    override fun close(window: Window) {
+        camera.delete()
+        renderingPath.delete()
     }
 
-    private static DeferredRenderingPath buildRenderingPath(ICamera camera, DeferredRenderNode renderNode,
-                                                            DirectionalLight light, CubeMapTexture cubeMap) {
-        final Fog fog = new Fog(Vector3.of(0), 700, 1000);
-
-        final DeferredRenderingPipeline renderPassesPipeline = new DeferredRenderingPipeline(
-                new DeferredGeometryPass(renderNode),
-                new LightRenderPass(light),
-                new SsaoRenderPass(),
-                new FogRenderPass(fog, FogDistance.XZ),
-                new SkyboxPostProcessor(cubeMap),
-                new FxaaPostProcessor()
-        );
-
-        return new DeferredRenderingPath(camera, renderPassesPipeline);
+    private fun buildWorld(): LowPolyWorld {
+        val heightGenerator: HeightGenerator = NoiseHeightGenerator(
+            MultipliedNoise2f(200, SpreadNoise2f(50,
+                LayeredNoise2f({ x: Float, y: Float -> SimplexNoise.noise(x, y) }, 5)))
+        )
+        val colourGenerator: ColourGenerator = NormalColourGenerator(Vector3.upward(),
+            NormalColour(0.5f, Vector3.of(.41f, .41f, .41f)),
+            NormalColour(1.0f, Vector3.of(.07f, .52f, .06f)))
+        val terrainFactory = LowPolyTerrainFactory(
+            DiamondMeshGenerator(64), heightGenerator,
+            colourGenerator, Vector2.of(256f, 256f)
+        )
+        return LowPolyWorld(terrainFactory)
     }
 
-    private static CubeMapTexture createCubeMap() throws IOException {
-        return new CubeMapTextureBuilder()
-                .positiveX("/assets/skybox/right.jpg")
-                .negativeX("/assets/skybox/left.jpg")
-                .positiveY("/assets/skybox/top.jpg")
-                .negativeY("/assets/skybox/bottom.jpg")
-                .positiveZ("/assets/skybox/front.jpg")
-                .negativeZ("/assets/skybox/back.jpg")
-                .create();
+    private fun buildCubeModel(world: World): Model3D {
+        val cubeInstance = instance()
+        cubeInstance.transform.scale[10f, 10f] = 10f
+        cubeInstance.transform.position[101f, world.getHeight(101f, 0f, 50f)] = 50f
+        val cubeMesh = mesh(arrayOf(cubeInstance),
+            ExamplesUtils.cubeVertices, ExamplesUtils.cubeIndices)
+        return Model3D(cubeMesh)
+    }
+
+    private fun buildDirectionalLight(): DirectionalLight {
+        val light = DirectionalLight()
+        light.direction[-1f, -1f] = -1f
+        light.colour[1f, 1f] = 1f
+        return light
+    }
+
+    private fun buildRenderingPath(camera: ICamera, renderNode: DeferredRenderNode,
+                                   light: DirectionalLight, cubeMap: CubeMapTexture): DeferredRenderingPath {
+        val fog = Fog(Vector3.of(0f), MAX_DISTANCE_CLIP * .7f, MAX_DISTANCE_CLIP)
+        val renderPassesPipeline = DeferredRenderingPipeline(
+            DeferredGeometryPass(renderNode),
+            LightRenderPass(light),
+            SsaoRenderPass(),
+            FogRenderPass(fog, FogDistance.XZ),
+            SkyboxPostProcessor(cubeMap),
+            FxaaPostProcessor()
+        )
+        return DeferredRenderingPath(camera, renderPassesPipeline)
+    }
+
+    private fun createCubeMap(): CubeMapTexture {
+        return CubeMapTextureBuilder()
+            .positiveX("/assets/skybox/right.jpg")
+            .negativeX("/assets/skybox/left.jpg")
+            .positiveY("/assets/skybox/top.jpg")
+            .negativeY("/assets/skybox/bottom.jpg")
+            .positiveZ("/assets/skybox/front.jpg")
+            .negativeZ("/assets/skybox/back.jpg")
+            .create()
     }
 }
