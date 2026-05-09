887450c92a7e7c5c87bec706fe0e14673d11f016
Saar Tako <saartako25@gmail.com>
2026-05-09 15:47:40 +0300

    fixed allocation strategy change after adding attachments

--- pe-core/src/main/java/org/saar/core/screen/ScreenBuilder.kt
+++ pe-core/src/main/java/org/saar/core/screen/ScreenBuilder.kt
@@ -1,6 +1,5 @@
 package org.saar.core.screen
 
-import org.saar.core.screen.image.ScreenImage
 import org.saar.core.screen.image.SimpleScreenImage
 import org.saar.lwjgl.opengl.constants.InternalFormat
 import org.saar.lwjgl.opengl.fbo.IFbo
@@ -8,13 +7,15 @@ import org.saar.lwjgl.opengl.fbo.attachment.Attachment
 import org.saar.lwjgl.opengl.fbo.attachment.allocation.AllocationStrategy
 import org.saar.lwjgl.opengl.fbo.attachment.allocation.MultisampledAllocationStrategy
 import org.saar.lwjgl.opengl.fbo.attachment.allocation.SimpleAllocationStrategy
+import org.saar.lwjgl.opengl.fbo.attachment.buffer.AttachmentBuffer
 import org.saar.lwjgl.opengl.fbo.attachment.buffer.RenderBufferAttachmentBuffer
 import org.saar.lwjgl.opengl.fbo.attachment.buffer.TextureAttachmentBuffer
 import org.saar.lwjgl.opengl.fbo.attachment.index.*
+import org.saar.lwjgl.opengl.texture.MutableTexture2D
 
 class ScreenBuilder(private val fbo: IFbo) {
 
-    private val attachments = ArrayList<AttachmentEntry>()
+    private val layers = ArrayList<Layer>()
 
     private var allocationStrategy: AllocationStrategy = SimpleAllocationStrategy()
 
@@ -25,66 +26,60 @@ class ScreenBuilder(private val fbo: IFbo) {
         return this
     }
 
-    fun addColourTexture(format: InternalFormat, read: Boolean, draw: Boolean): ScreenBuilder {
+    @JvmOverloads
+    fun addColourTexture(texture: MutableTexture2D = MutableTexture2D.create(),
+                         format: InternalFormat,
+                         read: Boolean,
+                         draw: Boolean): ScreenBuilder {
         val index = ColourAttachmentIndex(this.nextColourIndex++)
-        val buffer = TextureAttachmentBuffer(format)
-        val attachment = Attachment(buffer, this.allocationStrategy)
-        val image = SimpleScreenImage(attachment)
-        this.attachments.add(AttachmentEntry(index, image, read, draw))
+        this.layers.add(Layer(index, TextureAttachmentBuffer(texture, format), read, draw))
         return this
     }
 
     fun addColourRenderBuffer(format: InternalFormat, read: Boolean, draw: Boolean): ScreenBuilder {
         val index = ColourAttachmentIndex(this.nextColourIndex++)
-        val buffer = RenderBufferAttachmentBuffer(format)
-        val attachment = Attachment(buffer, this.allocationStrategy)
-        val image = SimpleScreenImage(attachment)
-        this.attachments.add(AttachmentEntry(index, image, read, draw))
+        this.layers.add(Layer(index, RenderBufferAttachmentBuffer(format), read, draw))
         return this
     }
 
-    fun addDepthTexture(format: InternalFormat): ScreenBuilder {
-        val buffer = TextureAttachmentBuffer(format)
-        val attachment = Attachment(buffer, this.allocationStrategy)
-        val image = SimpleScreenImage(attachment)
-        this.attachments.add(AttachmentEntry(DepthAttachmentIndex, image, read = false, draw = false))
+    @JvmOverloads
+    fun addDepthTexture(texture: MutableTexture2D = MutableTexture2D.create(), format: InternalFormat): ScreenBuilder {
+        this.layers.add(Layer(DepthAttachmentIndex, TextureAttachmentBuffer(texture, format),
+            read = false, draw = false))
         return this
     }
 
     fun addDepthRenderBuffer(format: InternalFormat): ScreenBuilder {
-        val buffer = RenderBufferAttachmentBuffer(format)
-        val attachment = Attachment(buffer, this.allocationStrategy)
-        val image = SimpleScreenImage(attachment)
-        this.attachments.add(AttachmentEntry(DepthAttachmentIndex, image, read = false, draw = false))
+        this.layers.add(Layer(DepthAttachmentIndex, RenderBufferAttachmentBuffer(format),
+            read = false, draw = false))
         return this
     }
 
     fun addStencilRenderBuffer(format: InternalFormat): ScreenBuilder {
-        val buffer = RenderBufferAttachmentBuffer(format)
-        val attachment = Attachment(buffer, this.allocationStrategy)
-        val image = SimpleScreenImage(attachment)
-        this.attachments.add(AttachmentEntry(StencilAttachmentIndex, image, read = false, draw = false))
+        this.layers.add(Layer(StencilAttachmentIndex, RenderBufferAttachmentBuffer(format),
+            read = false, draw = false))
         return this
     }
 
     fun addDepthStencilRenderBuffer(format: InternalFormat): ScreenBuilder {
-        val buffer = RenderBufferAttachmentBuffer(format)
-        val attachment = Attachment(buffer, this.allocationStrategy)
-        val image = SimpleScreenImage(attachment)
-        this.attachments.add(AttachmentEntry(DepthStencilAttachmentIndex, image, read = false, draw = false))
+        this.layers.add(Layer(DepthStencilAttachmentIndex, RenderBufferAttachmentBuffer(format),
+            read = false, draw = false))
         return this
     }
 
     fun build(): OffScreen {
         val screen = SimpleScreen(this.fbo)
 
-        attachments.forEach { screen.addScreenImage(it.index, it.image) }
+        layers.forEach { layer ->
+            val image = SimpleScreenImage(Attachment(layer.buffer, this.allocationStrategy))
+            screen.addScreenImage(layer.index, image)
+        }
 
-        val readEntries = attachments.filter { it.read }
+        val readEntries = layers.filter { it.read }
         require(readEntries.size <= 1) { "Multiple read attachments" }
         readEntries.firstOrNull()?.let { screen.setReadImages(it.index) }
 
-        val drawIndices = attachments.filter { it.draw }.map { it.index }
+        val drawIndices = layers.filter { it.draw }.map { it.index }
         if (drawIndices.isNotEmpty()) {
             screen.setDrawImages(*drawIndices.toTypedArray())
         }
@@ -94,10 +89,10 @@ class ScreenBuilder(private val fbo: IFbo) {
         return screen
     }
 
-    private data class AttachmentEntry(
+    private class Layer(
         val index: AttachmentIndex,
-        val image: ScreenImage,
+        val buffer: AttachmentBuffer,
         val read: Boolean,
-        val draw: Boolean
+        val draw: Boolean,
     )
 }
