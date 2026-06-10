27c3c1fd3dcfe55ce1c615016f22fb32745cae27
Saar Tako <saart@d-fendsolutions.com>
2026-06-10 17:26:39 +0300

    clean up forward rendering unwanted classes

--- pe-core/src/main/java/org/saar/core/renderer/forward/ForwardRenderingPipeline.kt
+++
@@ -1,7 +0,0 @@
-package org.saar.core.renderer.forward
-
-import org.saar.core.renderer.RenderingPathPipeline
-
-class ForwardRenderingPipeline(
-    override vararg val passes: ForwardRenderPass,
-) : RenderingPathPipeline<ForwardRenderingBuffers>
