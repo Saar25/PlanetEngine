34e5047ad22d0b3389d100a1d62a931e8f7e9e25
Saar Tako <saart@d-fendsolutions.com>
2026-06-12 12:19:42 +0300

    fixed null errors

--- pe-core/src/main/java/org/saar/core/renderer/RenderContext.kt
+++
@@ -1,6 +0,0 @@
-package org.saar.core.renderer
-
-import org.saar.core.camera.ICamera
-import org.saar.lwjgl.opengl.clipplane.ClipPlane
-
-class RenderContext @JvmOverloads constructor(val camera: ICamera?, val clipPlane: ClipPlane? = null)
