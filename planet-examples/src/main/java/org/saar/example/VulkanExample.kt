aa2c8b7d019cd6f57a11f240f923478c7bcb4fff
Saar Tako <saartako25@gmail.com>
2026-07-07 11:37:14 +0300

    create VkResult wrapper

--- planet-examples/src/main/java/org/saar/example/VulkanExample.kt
+++ planet-examples/src/main/java/org/saar/example/VulkanExample.kt
@@ -32,7 +32,7 @@ import org.saar.rhi.vulkan.depthstencil.toVulkan
 import org.saar.rhi.vulkan.inputassembly.toVulkan
 import org.saar.rhi.vulkan.multisample.toVulkan
 import org.saar.rhi.vulkan.rasterization.toVulkan
-import org.saar.rhi.vulkan.viewport.toVulkan
+import org.saar.rhi.vulkan.result.translateVulkanResult
 import java.io.*
 import java.nio.ByteBuffer
 import java.nio.IntBuffer
@@ -1210,47 +1210,6 @@ fun glslToSpirv(classPath: String, vulkanStage: Int): ByteBuffer {
     Shaderc.shaderc_result_release(res)
     Shaderc.shaderc_compiler_release(compiler)
     return resultBytes
-}
-
-/**
- * Translates a Vulkan `VkResult` value to a String describing the result.
- *
- * @param result the `VkResult` value
- *
- * @return the result description
- */
-fun translateVulkanResult(result: Int): String {
-    when (result) {
-        VK10.VK_SUCCESS -> return "Command successfully completed."
-        VK10.VK_NOT_READY -> return "A fence or query has not yet completed."
-        VK10.VK_TIMEOUT -> return "A wait operation has not completed in the specified time."
-        VK10.VK_EVENT_SET -> return "An event is signaled."
-        VK10.VK_EVENT_RESET -> return "An event is unsignaled."
-        VK10.VK_INCOMPLETE -> return "A return array was too small for the result."
-        KHRSwapchain.VK_SUBOPTIMAL_KHR -> return "A swapchain no longer matches the surface properties exactly, but can still be used to present to the surface successfully."
-        VK10.VK_ERROR_OUT_OF_HOST_MEMORY -> return "A host memory allocation has failed."
-        VK10.VK_ERROR_OUT_OF_DEVICE_MEMORY -> return "A device memory allocation has failed."
-        VK10.VK_ERROR_INITIALIZATION_FAILED -> return "Initialization of an object could not be completed for implementation-specific reasons."
-        VK10.VK_ERROR_DEVICE_LOST -> return "The logical or physical device has been lost."
-        VK10.VK_ERROR_MEMORY_MAP_FAILED -> return "Mapping of a memory object has failed."
-        VK10.VK_ERROR_LAYER_NOT_PRESENT -> return "A requested layer is not present or could not be loaded."
-        VK10.VK_ERROR_EXTENSION_NOT_PRESENT -> return "A requested extension is not supported."
-        VK10.VK_ERROR_FEATURE_NOT_PRESENT -> return "A requested feature is not supported."
-        VK10.VK_ERROR_INCOMPATIBLE_DRIVER -> return "The requested version of Vulkan is not supported by the driver or is otherwise incompatible for implementation-specific reasons."
-        VK10.VK_ERROR_TOO_MANY_OBJECTS -> return "Too many objects of the type have already been created."
-        VK10.VK_ERROR_FORMAT_NOT_SUPPORTED -> return "A requested format is not supported on this device."
-        KHRSurface.VK_ERROR_SURFACE_LOST_KHR -> return "A surface is no longer available."
-        KHRSurface.VK_ERROR_NATIVE_WINDOW_IN_USE_KHR -> return "The requested window is already connected to a VkSurfaceKHR, or to some other non-Vulkan API."
-        KHRSwapchain.VK_ERROR_OUT_OF_DATE_KHR -> return ("A surface has changed in such a way that it is no longer compatible with the swapchain, and further presentation requests using the "
-                + "swapchain will fail. Applications must query the new surface properties and recreate their swapchain if they wish to continue"
-                + "presenting to the surface.")
-
-        KHRDisplaySwapchain.VK_ERROR_INCOMPATIBLE_DISPLAY_KHR -> return ("The display used by a swapchain does not use the same presentable image layout, or is incompatible in a way that prevents sharing an"
-                + " image.")
-
-        EXTDebugReport.VK_ERROR_VALIDATION_FAILED_EXT -> return "A validation layer found an error."
-        else -> return String.format("%s [%d]", "Unknown", result)
-    }
 }
 
 fun allocateLayerBuffer(layers: Array<String>): PointerBuffer {
